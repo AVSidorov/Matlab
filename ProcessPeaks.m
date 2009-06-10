ec33424e5462c528e48f7e8784c8b8b03fcf43cf
Sidorov <Sidorov@ff67c247-4cd2-834c-9d9d-b0991a294cff>
2009-06-10 05:35:56 +0000

    git-svn-id: https://192.168.0.7:8443/svn/matlab@11 ff67c247-4cd2-834c-9d9d-b0991a294cff

--- ProcessPeaks.m
+++ ProcessPeaks.m
@@ -1,7 +1,7 @@
 function [Flow1,Flow2,Uloop,Etor,Wb,K_W,K_A]=ProcessPeaks(peaks);
 MeasAmplf=100;
-ClbrAmplf=11;
-ClbrAmplt=1775;
+ClbrAmplf=100;
+ClbrAmplt=2253;
 R=0.55;
 Flow1=[];
 Flow2=[];
@@ -22,32 +22,38 @@ TimeInt=MaxTime-MinTime;
 Time=0.5*(MinTime+MaxTime);
 
 [flow1,flow2,spectr]=AnalyzePeaks(peaks1);
+figure;
+    plot(spectr(:,1)*5.9/ClbrAmplt*(ClbrAmplf/MeasAmplf),spectr(:,2),'.-r');
+    grid on;
+    set(gca,'YScale','log');
+    
+assignin('base',['SpecT',num2str(round(Time/1000),'%2.0f'),'Av',num2str(round(TimeInt/1000),'%2.0f')],spectr);
 
-return;
-
-alpha=input('Input chord angel\n');
-if isempty(alpha)
-    alpha=0;
-end;
-Te=TeByAngelTime(alpha,Time);
-Ne=NeByAngelTime(alpha,Time);
-
-MaxwTab=MaxwTb('D:\!SCN\EField\maxw\MaxwSpectr.dat ');
-MaxwSpectr=MaxwSpByTe(Te,MaxwTab);
-
-[k_W,k_A,Wb1]=SpectrFit(spectr,MaxwSpectr,ClbrAmplt,ClbrAmplf,MeasAmplf);
-
-
-Et=4*Te*Ne/Wb1^2;
-Ul=Et*2*pi*R;
-fprintf('Uloop is %4.3f V, Etor is %4.3f V/m\n',Ul,Et);
- Flow1=[Flow1;[Time,flow1]];
- Flow2=[Flow2;[Time,flow2]];
- Uloop=[Uloop;[Time,Ul]];
- Etor=[Etor;[Time,Et]];
- Wb=[Wb;[Time,Wb1]];
- K_W=[K_W;[Time,k_W]];
- K_A=[K_A;[Time,k_A]];
+%return;
+% 
+% alpha=input('Input chord angel\n');
+% if isempty(alpha)
+%     alpha=0;
+% end;
+% Te=TeByAngelTime(alpha,Time);
+% Ne=NeByAngelTime(alpha,Time);
+% 
+% MaxwTab=MaxwTb('D:\!SCN\EField\maxw\MaxwSpectr.dat ');
+% MaxwSpectr=MaxwSpByTe(Te,MaxwTab);
+% 
+% [k_W,k_A,Wb1]=SpectrFit(spectr,MaxwSpectr,ClbrAmplt,ClbrAmplf,MeasAmplf);
+% 
+% 
+% Et=4*Te*Ne/Wb1^2;
+% Ul=Et*2*pi*R;
+% fprintf('Uloop is %4.3f V, Etor is %4.3f V/m\n',Ul,Et);
+%  Flow1=[Flow1;[Time,flow1]];
+%  Flow2=[Flow2;[Time,flow2]];
+%  Uloop=[Uloop;[Time,Ul]];
+%  Etor=[Etor;[Time,Et]];
+%  Wb=[Wb;[Time,Wb1]];
+%  K_W=[K_W;[Time,k_W]];
+%  K_A=[K_A;[Time,k_A]];
 continuestr=input('continue?[y]/n \n','s');
 if isempty(continuestr) 
     continuestr='y'; 
--- apparat_smooth.m
+++ apparat_smooth.m
@@ -6,15 +6,15 @@ function y=apparat_smooth(Spectr,K);
      Smooth(:,2)=Smooth(:,2)+Spectr(:,2).*SpectrFit(Spectr(:,1),Spectr(i,1),K);
  end;
 y=Smooth;
-function y=SpectrFit(x,W0,K);   % Poisson distribution
-X=K*x+1; X0=K*W0+1; 
-y=X;
-LargeBool=(X>20)&(X0>20);
-if not(isempty(y(LargeBool)))
-    XX=X(LargeBool);
-    y(LargeBool)=(1+1/12/X0-1/12./XX).*exp(XX-X0).*(X0./XX).^(XX+1/2);
-end; 
-if not(isempty(y(not(LargeBool))))
-    XX=X(not(LargeBool));
-    y(not(LargeBool))=(X0-1).^(XX-X0).*gamma(X0)./gamma(XX);
-end; 
+% function y=SpectrFit(x,W0,K);   % Poisson distribution
+% X=K*x+1; X0=K*W0+1; 
+% y=X;
+% LargeBool=(X>20)&(X0>20);
+% if not(isempty(y(LargeBool)))
+%     XX=X(LargeBool);
+%     y(LargeBool)=(1+1/12/X0-1/12./XX).*exp(XX-X0).*(X0./XX).^(XX+1/2);
+% end; 
+% if not(isempty(y(not(LargeBool))))
+%     XX=X(not(LargeBool));
+%     y(not(LargeBool))=(X0-1).^(XX-X0).*gamma(X0)./gamma(XX);
+% end; 
