7132dbe9c3f3f4ce50147f8087cd9257d6714477
Sidorov <Sidorov@ff67c247-4cd2-834c-9d9d-b0991a294cff>
2009-06-19 19:57:19 +0000

    git-svn-id: https://192.168.0.7:8443/svn/matlab@12 ff67c247-4cd2-834c-9d9d-b0991a294cff

--- ProcessingTrek.m
+++ ProcessingTrek.m
@@ -1,8 +1,8 @@
 function [peaks,trekMinus,StandardPulseFirst]=ProcessingTrek(FileName);
 
 
-Pass1=2;
-OverSt=1.1;         % noise regection threshold, in standard deviations 
+Pass1=1;
+OverSt=3;         % noise regection threshold, in standard deviations 
 
 [trek,ProcInt,ProcIntTime,StdVal]=PrepareTrek(FileName);
 if isempty(trek); return; end;
@@ -12,9 +12,9 @@ if isempty(trek); return; end;
 
 if isstr(FileName)
     [pathstr, name, ext, versn]=fileparts(FileName);
-    assignin('base',['StP',name],StandardPulseFirst);
+    assignin('base',['StP',name,'T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],StandardPulseFirst);
 else
-    assignin('base','StPulse',StandardPulseFirst);
+    assignin('base',['St32PT',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],StandardPulseFirst);
 end;
 
 [peaks,trekMinus]=GetPeaks(trek,Pass1,PeakSetFirst,StandardPulseFirst,StdVal*OverSt);
@@ -23,10 +23,11 @@ peaks(:,2)=peaks(:,2)+ProcIntTime(1);
 %peaks(:,1)=peaks(:,1)+ProcInt(1);
 if isstr(FileName)
     [pathstr, name, ext, versn]=fileparts(FileName);
-    assignin('base',['p',name],peaks);
+    assignin('base',['p',name,'T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],peaks);
 else
-    assignin('base','peaks',peaks);
-end;
+    assignin('base',['p32T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],peaks);
+end;    
+    
 
 [Flow1,Flow2,Uloop,Etor,Wb,K_W,K_A]=ProcessPeaks(peaks);
 
