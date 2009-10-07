06ebf1000ebb96855a9bb7aca7ef1d26db8064e6
Sidorov <Sidorov@ff67c247-4cd2-834c-9d9d-b0991a294cff>
2009-10-07 23:24:17 +0000

    git-svn-id: https://192.168.0.7:8443/svn/matlab@18 ff67c247-4cd2-834c-9d9d-b0991a294cff

--- ProcessingTrek.m
+++ ProcessingTrek.m
@@ -8,16 +8,20 @@ OverSt=3;         % noise regection threshold, in standard deviations
 if isempty(trek); return; end;
 
 
-[PeakSetFirst,StandardPulseFirst]=Tops(trek,0,StdVal*OverSt);
+[PeakSetFirst,StandardPulseFirst]=Tops(trek,1,StdVal*OverSt);
 
 if isstr(FileName)
     [pathstr, name, ext, versn]=fileparts(FileName);
     assignin('base',['StP',name,'T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],StandardPulseFirst);
 else
-    assignin('base',['StP40T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],StandardPulseFirst);
+    assignin('base',['StP18T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],StandardPulseFirst);
 end;
 
 [peaks,trekMinus]=GetPeaks(trek,Pass1,PeakSetFirst,StandardPulseFirst,StdVal*OverSt);
+StdValTM=std(trekMinus);
+fprintf('Initial Std=%3.3f\n',StdVal);
+fprintf('Final Std=%3.3f\n',StdValTM);
+
 
 peaks(:,2)=peaks(:,2)+ProcIntTime(1);
 %peaks(:,1)=peaks(:,1)+ProcInt(1);
@@ -25,7 +29,7 @@ if isstr(FileName)
     [pathstr, name, ext, versn]=fileparts(FileName);
     assignin('base',['p',name,'T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],peaks);
 else
-    assignin('base',['p40T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],peaks);
+    assignin('base',['p18T',num2str(round(ProcIntTime(1)/1000)),'d',num2str(round(ProcIntTime(end)/1000)),'ms'],peaks);
 end;    
     
  return;
