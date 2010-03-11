dbca7e90da080a02ba4ca5d6b078d3ae6201e9cb
Sidorov <Sidorov@ff67c247-4cd2-834c-9d9d-b0991a294cff>
2010-03-11 04:01:02 +0000

    git-svn-id: https://192.168.0.7:8443/svn/matlab@42 ff67c247-4cd2-834c-9d9d-b0991a294cff

--- HistOptima.m
+++ HistOptima.m
@@ -1,48 +1,99 @@
 function HistSet=HistOptima(A,X);
 
 [Hist1,HI,HS,HistSet1]=sid_hist(A,X,'max');
-[Hist10,HI,HS,HistSet10]=sid_hist(A,X,HS/1.1,HI);
 
 delta=1;
 delta1=0;
+
+HIST=[];
+delta1=[];
+delta2=[];
+delta3=[];
+delta4=[];
+delta5=[];
+delta6=[];
+delta7=[];
+
 figure;
+while HS<=HI
 
-while delta<1000
-    [Hist1,HI,HS,HistSet1]=sid_hist(A,X,HI,HI);
-    if HistSet10.HistN==HistSet1.HistN
-        delta1(end+1)=sum((Hist1(:,2)-Hist10(:,2)).^2)/HistSet10.HistN;
-    end;
-    subplot(4,1,1);
-    plot(Hist1(:,1),Hist1(:,2)/HistSet1.HI,'-b.');
+[Hist1,HI,HS,HistSet1]=sid_hist(A,X,HI,HI);
+Hist1(:,2)=Hist1(:,2)/HI;
+
+[Hist10,HI,HS,HistSet10]=sid_hist(A,X,HistSet1.Range/200,HI);
+Hist10(:,2)=Hist10(:,2)/HI;
+
+% HistSm=smooth(Hist10(:,2),round(HI/HS));
+ HistSm=smooth(Hist10(:,2),10);
+
+HistInt=interp1(Hist1(:,1),Hist1(:,2),Hist10(:,1),'pchip');
+
+Hist1D=diff(Hist1(:,2));
+Hist1DSh=circshift(Hist1D,1);
+F1=Hist1D.*Hist1DSh;
+
+Hist10D=diff(Hist10(:,2));
+Hist10DSh=circshift(Hist10D,1);
+F10=Hist10D.*Hist10DSh;
+
+M1=HistInt-Hist10(:,2);
+M1Sh=circshift(M1,1);
+F2=M1.*M1Sh;
+
+M2=HistSm-Hist10(:,2);
+M2Sh=circshift(M2,1);
+F3=M2.*M2Sh;
+
+if isempty(delta1)
+    HIST(:,1)=Hist10(:,2);
+    HistM=mean(HIST,2);
+    
+    delta1=size(find(F1<=0),1);
+    delta2=size(find(F10<=0),1);
+    delta3=sum((HistInt-Hist10(:,2)).^2)/HistSet10.HistN;
+    delta4=sum((HistM-Hist10(:,2)).^2)/HistSet10.HistN;
+    delta5=sum((HistSm-Hist10(:,2)).^2)/HistSet10.HistN;
+    delta6=size(find(F2<=0),1);
+    delta7=size(find(F3<=0),1);
+else
+    HIST(:,end+1)=Hist10(:,2);
+    HistM=mean(HIST,2);
+
+    delta1(end+1)=size(find(F1<=0),1);
+    delta2(end+1)=size(find(F10<=0),1);
+    delta3(end+1)=sum((HistInt-Hist10(:,2)).^2)/HistSet10.HistN;
+    delta4(end+1)=sum((HistM-Hist10(:,2)).^2)/HistSet10.HistN;
+    delta5(end+1)=sum((HistSm-Hist10(:,2)).^2)/HistSet10.HistN;
+    delta6(end+1)=size(find(F2<=0),1);
+    delta7(end+1)=size(find(F3<=0),1);
+end;
+
+    
+
+  subplot(3,1,1)
+    plot(Hist1(:,1),Hist1(:,2),'-b.');
     grid on; hold on;
-    plot(Hist10(:,1),Hist10(:,2)/HistSet10.HI,'-r.');
+    plot(Hist10(:,1),Hist10(:,2),'-r.');
+    plot(Hist10(:,1),HistSm,'-m.');
+    plot(Hist10(:,1),HistM,'-g.');
+    plot(Hist10(:,1),HistInt,'-k');
     hold off;
+  subplot(3,1,2)
+     plot(delta3,'-k.');
+     grid on; hold on;
+     plot(delta4,'-g.');
+     plot(delta5,'-m.');
+     hold off;
 
-    [Hist10,HI,HS,HistSet10]=sid_hist(A,X,HS/1.1,HI);
-%     HistInt(:,1)=Hist10(:,1);
-%     HistInt(:,2)=interp1(Hist1(:,1),Hist1(:,2),Hist10(:,1),'pchip');
-%     HistInt(:,3)=sqrt(HistInt(:,2));
-%     delta(end+1)=sum((Hist10(:,2)-HistInt(:,2)).^2)/HistSet10.HistN;
-  Hist1D=diff(Hist1(:,2));
-  Hist1DSh=circshift(Hist1D,1);
-  F=Hist1D.*Hist1DSh;
-  delta(end+1)=size(find(F<=0),1);
-
-  subplot(4,1,2);
-    plot(Hist1(:,1),Hist1(:,2)/HI);
-    grid on; hold on;
-%     plot(Hist10(:,1),Hist10(:,2),'-r.');
-%     plot(HistInt(:,1),HistInt(:,2),'-k');
-    subplot(4,1,3);
-        plot(delta,'-dr');
-        grid on; 
-        set(gca,'yscale','log');
-    subplot(4,1,4);
-        plot(delta1,'-dr');
-        grid on; 
-
+  subplot(3,1,3)
+     plot(delta1,'-b.');
+     grid on; hold on;
+     plot(delta2,'-r.');
+     plot(delta6,'-k.');
+     plot(delta7,'-m.');
+     hold off;
 
     pause;
-%     close(gcf);
     HI=HI/1.1;
+    clear HistInt;
 end;
