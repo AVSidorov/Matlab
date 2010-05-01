b45d7f8bbc57705cf36c01882a69937ed3a9f40b
Sidorov <Sidorov@ff67c247-4cd2-834c-9d9d-b0991a294cff>
2010-05-01 00:25:45 +0000

    git-svn-id: https://192.168.0.7:8443/svn/matlab@57 ff67c247-4cd2-834c-9d9d-b0991a294cff

---
+++ GasAmp.m
@@ -0,0 +1,16 @@
+function G=GasAmp(ParamSet);
+%Gives AmpLitude of 5.9keV in ADC Counts;
+a=0.005;
+b=0.9;
+Amp=ParamSet.Amp;
+%C=0.007576780;
+ C=0.03111;
+Q=ParamSet.charge;
+Vo=ParamSet.HV;
+P=760*(1+ParamSet.P);
+
+V=Vo-C*Q;
+X=V/log(b/a)/a./P;
+S=a*P;
+K=V/log(b/a);
+G=exp(K*(0.685*X-28.8-0.75*X/S)/1.05e3)*Amp;
--- TrekCharge.m
+++ TrekCharge.m
@@ -5,13 +5,14 @@ TrekSet=TrekSetIn;
 Tdr=895; %Full ions drift time us
 NPeak=size(TrekSet.peaks,1);
 TrekSet.charge=zeros(NPeak,1);
-if NPeak>0
-    StartTime=TrekSet.peaks(:,2)-Tdr;
-    StartTime(StartTime<0)=0;
-    % StartInd=ones(NPeak,1);
-    for i=1:NPeak
-        StartInd=find(TrekSet.peaks(:,2)>StartTime(i),1,'first');
-        TrekSet.charge(i)=sum(TrekSet.peaks(StartInd:i,5));
-    end;
-end;
+ if NPeak>0
+     StartTime=TrekSet.peaks(:,2)-Tdr;
+     StartTime(StartTime<0)=0;
+     % StartInd=ones(NPeak,1);
+     for i=1:NPeak
+         StartInd=find(TrekSet.peaks(:,2)>StartTime(i),1,'first');
+         t=TrekSet.peaks(StartInd:i,2)-TrekSet.peaks(i,2);     
+         TrekSet.charge(i)=sum(TrekSet.peaks(StartInd:i,5).*exp(t/(Tdr/5)));
+     end;
+ end;
 toc;
