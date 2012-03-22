8f8bc3b50e4f8efed3fdcad82e0edba88e56c29a
Anton Sidorov <sidorov@mail.ioffe.ru>
2012-03-22 17:18:51 +0400

    added deleting by Tag in TrekPlotTime

--- TrekPlotTime.m
+++ TrekPlotTime.m
@@ -24,13 +24,27 @@ xlabel('Time,  {\mu}s');
 ylabel('ADC counts');
 grid on;
 hold on;
+
+h=findobj(hAxes,'Tag','TrekLine');
+if ~isempty(h)
+    delete(h);
+end;
 plot(TrekSet.StartTime+[0:TrekSet.size-1]*TrekSet.tau,TrekSet.trek,'Tag','TrekLine');
+
 axis([TrekSet.StartTime,TrekSet.StartTime+(TrekSet.size-1)*TrekSet.tau,TrekSet.MinSignal,TrekSet.MaxSignal]);
 if isfield(TrekSet,'SelectedPeakInd');
+    h=findobj(hAxes,'Tag','SelectedPeakIndLine');
+    if ~isempty(h)
+        delete(h);
+    end;
     plot(TrekSet.StartTime+(TrekSet.SelectedPeakInd-1)*TrekSet.tau,TrekSet.trek(TrekSet.SelectedPeakInd),'.r','Tag','SelectedPeakIndLine');
 end;
 if isfield(TrekSet,'peaks')
     if not(isempty(TrekSet.peaks))
+        h=findobj(hAxes,'Tag','PeaksLine');
+        if ~isempty(h)
+            delete(h);
+        end;
         plot(TrekSet.peaks(:,2),TrekSet.peaks(:,4)+TrekSet.peaks(:,5),'>r','Tag','PeaksLine');
     end;
 end;
