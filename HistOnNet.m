8ca5b5d33fc183f21b53a219d14e43f2697024f9
Anton Sidorov <sidorov@mail.ioffe.ru>
2012-03-29 19:43:32 +0400

    HistOnNet fixed

--- HistOnNet.m
+++ HistOnNet.m
@@ -1,7 +1,9 @@
 function Hist=HistOnNet(A,net)
 for i=1:numel(net)-1
     Hist(i,1)=(net(i)+net(i+1))/2;
-    Hist(i,2)=numel(find(A>net(i)&A<=net(i+1)));
-    Hist(i,3)=sqrt(Hist(i,2));
+    N=numel(find(A>net(i)&A<=net(i+1)));
+    err=1/sqrt(N);
+    Hist(i,2)=N/(net(i+1)-net(i));
+    Hist(i,3)=Hist(i,2)*err;
 end;
     
