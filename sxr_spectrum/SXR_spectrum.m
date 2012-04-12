5a90916e39a1b16a6288eb4a6ff7389b2dda91f2
Anton Sidorov <sidorov@mail.ioffe.ru>
2012-04-12 22:58:28 +0400

    constructor alpha version made

--- sxr_spectrum/SXR_spectrum.m
+++ sxr_spectrum/SXR_spectrum.m
@@ -7,7 +7,7 @@ classdef SXR_spectrum
         Ne;
         Ni;
         Zeff;
-        PoissonK;
+        K;
         Be;
         P10;
         Transition;
@@ -23,9 +23,11 @@ classdef SXR_spectrum
                 eval(['obj.',varargin{1+2*(i-1)},'=varargin{2*i};']);
                end;
             end;
-            Te=[];
+            obj.Ne=1e19;
             if isempty(obj.Te)
                 Te=input('input electron temperature in eV. Default is 500eV\n');
+            else
+                Te=obj.Te;
             end;
             if isempty(Te)
                 obj.Te=500;
@@ -35,6 +37,34 @@ classdef SXR_spectrum
             if isempty(obj.Ni)
                 [obj.Ni,obj.Zeff]=Species_by_Zeff;
             end;
+            try
+                S=load('Be.mat', 'Be');
+                obj.Be=S.Be;
+            catch err
+            end
+            try
+                S=load('P10.mat', 'P10');
+                obj.P10=S.P10;
+            catch err
+            end
+            obj.Transition=obj.Be;
+            obj.Transition(:,2)=obj.Be(:,2).*(1-obj.P10(:,2));
+            obj.Spec=SXR_spectr_calc(obj.Ne,obj.Te,'Ni',obj.Ni,'Ew',obj.Be(:,1));
+            obj.SpecBe=obj.Spec;
+            obj.SpecBe(:,2)=obj.Spec(:,2).*obj.Transition(:,2);
+            
+            if isempty(obj.K)
+                K=input('Input poisson coefficent or FWHM in procents/100 (0<FWHM<1). Default is 15%\n');
+            else
+                K=obj.K;
+            end;
+            if isempty(K)
+                obj.K=2*5.9/(0.15*5.9)^2;
+            elseif K<1
+                obj.K=2*5.9/(0.15*5.9)^2;
+            end;
+            obj.SpecBeSm=obj.SpecBe;
+            obj.SpecBeSm(:,2)=Poisson_smooth([obj.SpecBe(:,1)/1000,obj.SpecBe(:,2)],obj.K);
         end
     end
 end %classdef
