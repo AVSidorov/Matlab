73c98452d5156a5d48fdff3ea5508542e1dcc4df
Anton Sidorov <sidorov@mail.ioffe.ru>
2012-04-12 13:04:45 +0400

    Varargin input added

--- SXR_spectr_calc.m
+++ SXR_spectr_calc.m
@@ -1,28 +1,48 @@
-function [Spectr,Ni]=SXR_spectr_calc(ne,Te,Ni);
+function [Spectr,Ni]=SXR_spectr_calc(ne,Te,varargin)
+%calculates Thermal Bremsstrahlung and recombination spectra 
+Ni=[];
+Ew=[];
 
-Ew_arr=250:15:7750;
-
-if nargin<3
-    %[Ni,Zeff]=Zeff_calc_manual(ne);
-    Ni=Species_by_Zeff;
+nargsin=size(varargin,2);
+if ~isempty(varargin)&&mod(nargsin,2)~=0
+    disp('incorrect number of input arguments');
+    return;
+else
+   for i=1:fix(nargsin/2) 
+    eval([varargin{1+2*(i-1)},'=varargin{2*i};']);
+%     switch varargin(1+2*(i-1))
+%         case 'Ni'
+%             Ni=varargin(2*i);
+%         case 'Ew'
+%             Ew=varagin(2*i);
+%       end;
+%    end;
+    end;
 end;
 
-Jb=zeros(size(Ew_arr));
-Jr=zeros(size(Ew_arr));
+if isempty(Ni)
+    Ni=Species_by_Zeff;
+end;
+if isempty(Ew)
+    Ew=250:10:10000;
+end;
+
+Jb=zeros(size(Ew));
+Jr=zeros(size(Ew));
 for i=1:max(size(Ni))
     Z=Ni(i,1);
     ni=Ni(i,2)*ne;
-    [jb(:,i),jr(:,i)]=J_calc(ne,ni,Z,Te,Ew_arr);
+    [jb(:,i),jr(:,i)]=J_calc(ne,ni,Z,Te,Ew);
     Jb(:)=Jb(:)+jb(:,i);
     Jr(:)=Jr(:)+jr(:,i);
 end;    
 
 % figure;
-    semilogy(Ew_arr,Jb(:),'-g','LineWidth',2);
+    semilogy(Ew,Jb(:),'-g','LineWidth',2);
     hold on; grid on;
-    semilogy(Ew_arr,Jr(:),'-b','LineWidth',2);
-    semilogy(Ew_arr,Jb(:)+Jr(:),'-r','LineWidth',3);
-    semilogy(Ew_arr,jb(:,find(Ni(:,1)==1)),'-c','LineWidth',2);
+    semilogy(Ew,Jr(:),'-b','LineWidth',2);
+    semilogy(Ew,Jb(:)+Jr(:),'-r','LineWidth',3);
+    semilogy(Ew,jb(:,find(Ni(:,1)==1)),'-c','LineWidth',2);
         
-Spectr(:,1)=Ew_arr;
+Spectr(:,1)=Ew;
 Spectr(:,2)=Jb(:)+Jr(:);
