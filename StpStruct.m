5f57235e91c656850df361dbe7ca4db6091e7a91
Anton Sidorov <sidorov@mail.ioffe.ru>
2012-06-13 18:58:15 +0400

    StpStruct some fixing and modifications (IndPos IndNeg etc.)

--- StpStruct.m
+++ StpStruct.m
@@ -8,27 +8,35 @@ tic;
 disp('>>>>>>>>StpStruct started');
 
 if nargin<1
-    StandardPulseFile='D:\!SCN\StandPeakAnalys\StPeakAmp4_20ns_1.dat';
+    StandardPulseFile='D:\!SCN\StandPeakAnalys\StPeakAmp4_20ns_5.dat';
 end;
+if isstr(StandardPulseFile) 
  if exist(StandardPulseFile,'file');
          StandardPulse=load(StandardPulseFile);
- else
-     disp('Standart pulse file not found\n');
+ else     
+     disp('Standart pulse file not found');
      STP=[];
      return;
  end;
+else
+    StandardPulse=StandardPulseFile;
+    if size(StandardPulse,2)>size(StandardPulse,1)
+        StandardPulse=StandardPulse'; %make vertical
+    end;
+end;
 
 Plot=false;
 
 %MaxInd=find(StandardPulse==max(StandardPulse));
 MaxInd=find(StandardPulse==1); %Standard Pulse must be normalized by Amp s
-BckgFitInd=find(StandardPulse==0);%Standard Pulse must have several zero point at front end and las zero point
-BckgFitInd(end)=[];
-BckgFitN=numel(BckgFitInd); 
+FirstNotZero=find(abs(StandardPulse),1,'first');%Standard Pulse must have several zero point at front end and las zero point
+BckgFitN=FirstNotZero-1; 
 FrontN=MaxInd-BckgFitN;
 TailInd=find(StandardPulse<=0);
 TailInd(TailInd<MaxInd)=[];
 TailInd=TailInd(1);
+LastNotZero=find(abs(StandardPulse),1,'last');
+ZeroTailN=numel(StandardPulse)-LastNotZero;
 
 Stp=StandardPulse;
 StpR=circshift(StandardPulse,1);
@@ -36,6 +44,11 @@ StpL=circshift(StandardPulse,-1);
 maxI=find(Stp>StpR&Stp>StpL);
 maxI(maxI>=MaxInd+2)=[];
 maxN=numel(maxI); 
+
+IndNegativeTail=find(Stp<0);
+IndPositiveTail=find(Stp>0);
+IndPositiveTail(IndPositiveTail<IndNegativeTail(1))=[];
+IndPulse=1:IndNegativeTail(1)-1;
 
 STP.Stp=StandardPulse;
 STP.size=numel(StandardPulse);
@@ -47,7 +60,11 @@ STP.maxN=1;
 STP.maxI=MaxInd;
 STP.max=1;
 STP.TailInd=TailInd;
+STP.ZeroTailN=ZeroTailN;
 STP.StandardPulseFile=StandardPulse;
+STP.IndNegativeTail=IndNegativeTail;
+STP.IndPositiveTail=IndPositiveTail;
+STP.IndPulse=IndPulse;
 %%
 toc;
 %%
