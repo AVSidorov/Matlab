883540ba518c9fdf267ae91974574cfa3a09909e
Anton Sidorov <sidorov@mail.ioffe.ru>
2013-07-18 17:07:46 +0400

    TrekSubtractManual. Added choise of quality fitting, and changed trek interval for showing.

--- TrekSubtractManual.m
+++ TrekSubtractManual.m
@@ -17,8 +17,8 @@ if isempty(SubtractInd)
 end;
 SubtractIndPulse=SubtractInd-FIT.MaxInd+STP.MaxInd;
 
-% x=Ind-STP.size:Ind+STP.size;
-% x(x<1|x>TrekSet.size)=[];
+x=SubtractInd(1)-STP.size:SubtractInd(end)+STP.size;
+x(x<1|x>TrekSet.size)=[];
 % Xbool=false(size(TrekSet.trek));
 % Xbool(x)=true;
 % SelectedBool=false(size(TrekSet.trek));
@@ -36,21 +36,23 @@ BadInd2=Ind(TrekSet1.trek(Ind)-FIT.B<=-TrekSet.Threshold);
 
 ts=figure;
 grid on; hold on;
-plot(SubtractInd,TrekSet.trek(SubtractInd));
-plot(SubtractInd,TrekSet1.trek(SubtractInd),'k');
+plot(x,TrekSet.trek(x));
+plot(x,TrekSet1.trek(x),'k');
 plot([1:FIT.FitPulseN]+FIT.MaxInd-STP.MaxInd,FIT.FitPulse*FIT.A+FIT.B,'r');
 % axis([FIT.FitInd(1),STP.TailInd(end)+FIT.MaxInd-STP.MaxInd,...
 %     min([min(TrekSet.trek(bool)),min(TrekSet1.trek(bool)),-TrekSet.Threshold]),...
 %     max([max(TrekSet.trek(bool)),max(TrekSet1.trek(bool))])]);
 plot(BadInd1,TrekSet1.trek(BadInd1),'.r');
 plot(BadInd2,TrekSet1.trek(BadInd2),'+r');
-plot([FIT.FitInd(1),SubtractInd(end)],FIT.B+[TrekSet.Threshold,TrekSet.Threshold],'g');
-plot([FIT.FitInd(1),SubtractInd(end)],FIT.B+[-TrekSet.Threshold,-TrekSet.Threshold],'g');
-s=input('Subtract? If input is not empty, then black trek,else Default blue line \n','s');
+plot([x(1),x(end)],FIT.B+[TrekSet.Threshold,TrekSet.Threshold],'g');
+plot([x(1),x(end)],FIT.B+[-TrekSet.Threshold,-TrekSet.Threshold],'g');
+s=input('Subtract? If input is not empty, then black trek,else Default blue line\n If leter is ''d/D'' then this pulse will be marked as good fitted\n','s');
 if not(isempty(s))
     Subtracted=true;
     TrekSet=TrekSet1;
-%     TrekSet.peaks(end,7)=0;
+    if lower(s)=='d'
+      TrekSet.peaks(end,7)=0;
+    end;
 end;
 if not(isempty(ts))&&ishandle(ts)
     close(ts);
