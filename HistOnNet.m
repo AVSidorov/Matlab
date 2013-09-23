1cf66399edccd0fae421272f51a0cec09f4fa26f
Anton Sidorov <sidorov@mail.ioffe.ru>
2013-09-23 11:04:13 +0400

    HistOnNet make faster. Memory allocation at start and other

--- HistOnNet.m
+++ HistOnNet.m
@@ -1,9 +1,10 @@
 function Hist=HistOnNet(A,net)
+tic;
+Hist=zeros(numel(net)-1,3);
+Hist(:,1)=(net(1:end-1)+net(2:end))/2;
 for i=1:numel(net)-1
-    Hist(i,1)=(net(i)+net(i+1))/2;
     N=numel(find(A>net(i)&A<=net(i+1)));
-    err=1/sqrt(N);
     Hist(i,2)=N/(net(i+1)-net(i));
-    Hist(i,3)=Hist(i,2)*err;
 end;
-    
+Hist(:,3)=sqrt(Hist(:,2));    
+toc;
