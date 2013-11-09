959654fae6ad17eef5133caf035f2506517e3751
Anton Sidorov <sidorov@mail.ioffe.ru>
2013-11-09 04:39:22 +0400

    TrekSDDFitByMove. Additional conditions to avoid bad initial points for PeakOnFront fitting

--- TrekSDDFitByMove.m
+++ TrekSDDFitByMove.m
@@ -27,6 +27,7 @@ FitPulse=TrekSet.STP.Stp(FitIndPulse);
 FitPulseL=TrekSet.STP.Stp(FitIndPulseL);
 A=zeros(TrekSet.size,3);
 B=zeros(TrekSet.size,3);
+good=false(TrekSet.size,2);
 if nargin<2
     Ind=1;
 else
@@ -41,12 +42,14 @@ while T<Tmax&&Ind<TrekSet.size-STP.MaxInd;
 FitInd=FitIndPulse+Ind-1;
 p=polyfit(FitPulse,TrekSet.trek(FitInd),1);
 khi=sqrt(sum(((TrekSet.trek(FitInd)-p(1)*FitPulse-p(2))/TrekSet.StdVal).^2)/N);
+good(Ind,1)=all(TrekSet.trek(FitInd)-p(1)*FitPulse-p(2)<TrekSet.StdVal*TrekSet.OverSt);
 A(Ind,1:2)=p;
 A(Ind,3)=khi;
 
 FitInd=FitIndPulseL+Ind-1;
 p=polyfit(FitPulseL,TrekSet.trek(FitInd),1);
 khi=sqrt(sum(((TrekSet.trek(FitInd)-p(1)*FitPulseL-p(2))/TrekSet.StdVal).^2)/N);
+good(Ind,2)=all(TrekSet.trek(FitInd)-p(1)*FitPulseL-p(2)<TrekSet.StdVal*TrekSet.OverSt);
 B(Ind,1:2)=p;
 B(Ind,3)=khi;
 
@@ -83,7 +86,7 @@ if all(Flag(1:2))&&any(Flag(3:4))&&any(Flag(5:6))
     FIT.FitPulse=TrekSet.STP.Stp;
     FIT.FitPulseN=TrekSet.STP.size;
     FIT.BGLineFit=[0,0];
-    if all(Flag(3:4))&&range(I)<3&&abs(A(I(1),1)-B(I(1),1))<TrekSet.OverSt*TrekSet.StdVal
+    if all(Flag(3:4))&&good(I(1),1)&&good(I(2),2)&&range(I)<3&&abs(A(I(1),1)-B(I(1),1))<TrekSet.OverSt*TrekSet.StdVal
         FIT.MaxInd=round(mean(I(I>0))-1+TrekSet.STP.MaxInd);
         FIT.A=B(FIT.MaxInd+1-TrekSet.STP.MaxInd,1);
         FIT.B=B(FIT.MaxInd+1-TrekSet.STP.MaxInd,2);
