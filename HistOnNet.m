dee5dce756c74d6dcb71ac4d117afbc9b4cd965b
Anton Sidorov <sidorov@mail.ioffe.ru>
2013-11-13 14:46:12 +0400

    HistOnNet. Make normalization to full number of peaks in channel.

--- HistOnNet.m
+++ HistOnNet.m
@@ -2,9 +2,16 @@ function Hist=HistOnNet(A,net)
 tic;
 Hist=zeros(numel(net)-1,3);
 Hist(:,1)=(net(1:end-1)+net(2:end))/2;
+Nmax=0;
 for i=1:numel(net)-1
     N=numel(find(A>net(i)&A<=net(i+1)));
     Hist(i,2)=N/(net(i+1)-net(i));
+    if N>Nmax
+     Nmax=N;
+     Imax=i;
+    end;
 end;
+k=(net(Imax+1)-net(Imax));
+Hist(:,2)=Hist(:,2)*k;
 Hist(:,3)=sqrt(Hist(:,2));    
 toc;
