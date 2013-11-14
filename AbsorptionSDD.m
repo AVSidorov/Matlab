e9b2a9e581b48a221a9dd90f85c299d03142375f
Anton Sidorov <sidorov@mail.ioffe.ru>
2013-11-14 14:25:20 +0400

    AbsorptionSDD. Introduced Si.

--- AbsorptionSDD.m
+++ AbsorptionSDD.m
@@ -1,20 +1,37 @@
-function T=AbsorptionSDD(FilterThikness,E)
+function [A,TBe,TSi]=AbsorptionSDD(FilterThikness,E)
 r0=2.817940289458e-15;%m
 h=4.13566751691e-15; %eV*s
 c=299792458;%m/s
 roBe=1.848; %g/cm3
 mmolBe=9.012182; %g/mol;
-n=roBe/mmolBe*6.0221412927e23; % 1/cm3
-n=n*1e6; %1/m3
+Na=6.0221412927e23; %Avogadro number
+nBe=roBe/mmolBe*Na; % 1/cm3
+nBe=nBe*1e6; %1/m3
+mmolSi=28.0856;%g/mol
+roSi=2.33;%g/cm3
+nSi=roSi/mmolSi*Na; % 1/cm3
+nSi=nSi*1e6; %1/m3
+
 d=FilterThikness;
+dSi=450;
+
 ffBe=load('d:\!scn\efield\be\be.dat','-ascii');
-% ffSi=load('si.dat','-ascii');
 if nargin<2
  E=ffBe(:,1);
 end;
+ffSi=load('d:\!scn\efield\be\si.dat','-ascii');
+
 f=E/h;
 l=c./f;
-f2=interp1(ffBe(:,1),ffBe(:,3),E,'linear');
-mu=2*r0*l.*f2;
-T(:,1)=E;
-T(:,2)=exp(-n*mu*d*1e-6);
+
+fBe=interp1(ffBe(:,1),ffBe(:,3),E,'linear');
+muBe=2*r0*l.*fBe;
+TBe(:,1)=E;
+TBe(:,2)=exp(-nBe*muBe*d*1e-6);
+
+fSi=interp1(ffSi(:,1),ffSi(:,3),E,'linear');
+muSi=2*r0*l.*fSi;
+TSi(:,1)=E;
+TSi(:,2)=exp(-nSi*muSi*dSi*1e-6);
+A(:,1)=E;
+A(:,2)=TBe(:,2).*(1-TSi(:,2));
