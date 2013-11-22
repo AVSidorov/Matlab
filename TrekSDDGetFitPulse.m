d7329f5d8a31008bbd5e4eb5ab39c8f6547ea1e5
Anton Sidorov <sidorov@mail.ioffe.ru>
2013-11-22 18:57:41 +0400

    TrekSDDGetFitPulse. GREAT optimization. (min 3 times)

--- TrekSDDGetFitPulse.m
+++ TrekSDDGetFitPulse.m
@@ -3,7 +3,7 @@ Stp=STP.FinePulse;
 StpN=STP.size;
 TimeInd=STP.TimeInd;
 
-FitPulse=interp1(TimeInd,Stp,[1:StpN]'+Shift,'spline',0);
+FitPulse=interp1(TimeInd,Stp,[1:StpN]'+Shift,'linear',0);
 FitPulse(FitPulse(1:STP.MaxInd)<0)=0;
 bool=FitPulse<0;
 bool(1:STP.IndPositiveTail(3))=false;
