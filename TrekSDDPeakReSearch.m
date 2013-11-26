02cb763e2d8a9998a0e8a0e534876daf7f5575bf
Anton Sidorov <sidorov@mail.ioffe.ru>
2013-11-26 13:14:44 +0400

    TrekSDDPeakReSearch. FixedBug if return from function without new peak search

--- TrekSDDPeakReSearch.m
+++ TrekSDDPeakReSearch.m
@@ -2,24 +2,26 @@ function TrekSet=TrekSDDPeakReSearch(TrekSetIn,FIT)
 tic;
 disp('>>>>>>>>TrekPeakReSearch started');
 
-TrekSet=TrekSetIn;
 
-StpSet=TrekSet.STP;
+StpSet=TrekSetIn.STP;
 
 if nargin<2
-    SubtractInd=[1:TrekSet.size]';
+    SubtractInd=[1:TrekSetIn.size]';
 else
     SubtractInd=[1:FIT.FitPulseN]+FIT.MaxInd-StpSet.MaxInd;
     SubtractInd=[SubtractInd(1)-StpSet.MaxInd:SubtractInd(end)]';
-    SubtractInd=SubtractInd(SubtractInd<=TrekSet.size&SubtractInd>=1);
+    SubtractInd=SubtractInd(SubtractInd<=TrekSetIn.size&SubtractInd>=1);
 end;
+TrekSetIn.SelectedPeakInd(TrekSetIn.SelectedPeakInd>=SubtractInd(1)&TrekSetIn.SelectedPeakInd<=SubtractInd(end))=[];
+TrekSet=TrekSetIn;
+
 if numel(SubtractInd)<StpSet.MaxInd||...
     range(TrekSet.trek(SubtractInd))<TrekSet.Threshold
     return;
 end;
 
 
- TrekSetIn.SelectedPeakInd(TrekSet.SelectedPeakInd>=SubtractInd(1)&TrekSet.SelectedPeakInd<=SubtractInd(end))=[];
+ 
   
  TrekSet.Plot=false;
  TrekSet.trek=[TrekSet.trek(SubtractInd)];
