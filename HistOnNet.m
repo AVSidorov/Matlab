91a3b7bbf2e754e08d0a055553f66354905142e7
Anton Sidorov <sidorov@mail.ioffe.ru>
2013-12-24 18:48:58 +0400

    HistOnNet. Bug fixing (not finally)

--- HistOnNet.m
+++ HistOnNet.m
@@ -3,6 +3,7 @@ tic;
 Hist=zeros(numel(net)-1,3);
 Hist(:,1)=(net(1:end-1)+net(2:end))/2;
 Nmax=0;
+Imax=[];
 for i=1:numel(net)-1
     N=numel(find(A>net(i)&A<=net(i+1)));
     Hist(i,2)=N/(net(i+1)-net(i));
@@ -11,7 +12,11 @@ for i=1:numel(net)-1
      Imax=i;
     end;
 end;
-k=(net(Imax+1)-net(Imax));
+if ~isempty(Imax)
+    k=(net(Imax+1)-net(Imax));
+else 
+    k=1;
+end;
 Hist(:,2)=Hist(:,2)*k;
 Hist(:,3)=sqrt(Hist(:,2));    
 toc;
