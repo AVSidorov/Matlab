47a75b61a7c718b4ec13631bb568ceb907e4d5ab
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-04-23 16:58:20 +0400

    ZeroCross Determination zero crossing time point by linear interpolation

--- DensityPhaseZeroCross.m
+++ DensityPhaseZeroCross.m
@@ -4,27 +4,27 @@ Plot=true;
 
 Fbase=421748;
 Tbase=1/Fbase;
-TbaseTact=Tbase/tau;
 
 
-Base=sin(2*pi*Fbase*[0:tau:(numel(trek)-1)*tau]');
 
-Ind1=find(trek.*circshift(trek,-1)<=0);
-i=find(trek(Ind1)<0&trek(Ind1+1)>0,1,'first');
-Ind1=Ind1(i:end);
+IndPreCross=find(trek.*circshift(trek,-1)<=0);
+i=find(trek(IndPreCross)<0&trek(IndPreCross+1)>0,1,'first');
+IndPreCross=IndPreCross(i:end);
+IndCross=IndPreCross-trek(IndPreCross)./(trek(IndPreCross+1)-trek(IndPreCross));
 
+TimeCross=(IndCross-1)*tau;
+TimeCrossBase=[0:Tbase/2:(numel(trek)-1)*tau]';
 
-Ind2=find(Base.*circshift(Base,-1)<=0);
+N=min([numel(TimeCross),numel(TimeCrossBase)]);
+Ph=(TimeCrossBase(1:N)-TimeCross(1:N));
+Ph=Ph/Tbase*2*pi;
 
-N=min([numel(Ind1),numel(Ind2)]);
-Ph=(Ind1(1:N)-Ind2(1:N));
 Ph=Ph-Ph(1);
 [m,mi]=max(abs(Ph));
 Ph=Ph*sign(Ph(mi));
-Ph=Ph/TbaseTact*2*pi;
-Ph=Ph/2/pi;
+
 Phase(1:N,1)=[0:N-1]*Tbase/2;
-Phase(:,2)=Ph;
+Phase(:,2)=Ph/(2*pi);
 if Plot
     figure;
     plot(Phase(:,1),Phase(:,2),'r');
