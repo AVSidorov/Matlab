5075fec34617b91f684ae84dc916231fc9cc2a94
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-04-24 14:06:35 +0400

    CrossZero works on unfiltered trek, but slow

--- DensityPhaseZeroCross.m
+++ DensityPhaseZeroCross.m
@@ -4,16 +4,28 @@ Plot=true;
 
 Fbase=421748;
 Tbase=1/Fbase;
+WinWidth=pi/2; % window for reducing point in noise case
 
 
 
-IndPreCross=find(trek.*circshift(trek,-1)<=0);
+IndPreCross=find(trek.*circshift(trek,-1)<0|trek==0); %<= gives double indexing
 if IndPreCross(end)==numel(trek) IndPreCross(end)=[]; end;
 i=find(trek(IndPreCross)<0&trek(IndPreCross+1)>0,1,'first');
 IndPreCross=IndPreCross(i:end);
 IndCross=IndPreCross-trek(IndPreCross)./(trek(IndPreCross+1)-trek(IndPreCross));
 
+i=1;
+while i<numel(IndCross)
+    ind=find(IndCross>=IndCross(i)&IndCross<IndCross(i)+Tbase*WinWidth/(2*pi)/tau);
+    if numel(ind)>1
+        IndCross(i)=mean(IndCross(ind));
+        IndCross(ind(2:end))=[];
+    end;
+    i=i+1;
+end;
+
 TimeCross=(IndCross-1)*tau;
+
 TimeCrossBase=[0:Tbase/2:(numel(trek)-1)*tau]';
 
 N=min([numel(TimeCross),numel(TimeCrossBase)]);
