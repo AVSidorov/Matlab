ad9ce6b574f2feb0eff7c1e38fc41fa577fceea0
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-05-14 14:10:36 +0400

    TrekSDDNoise added. Function fills fields in TrekSet. Noise is determinated by NoiseHist

---
+++ TrekSDD/TrekSDDNoise.m
@@ -0,0 +1,30 @@
+function TrekSet=TrekSDDNoise(TrekSet,varargin)
+StartTime=0;
+EndTime=(TrekSet.size-1)*TrekSet.tau;
+nargsin=size(varargin,2);
+if ~isempty(varargin)&&mod(nargsin,2)~=0
+    disp('incorrect number of input arguments');
+    TrekSet.type=0;
+    return;
+else
+    for i=1:fix(nargsin/2) 
+        eval([varargin{1+2*(i-1)},'=varargin{2*i};']);
+    end;
+end;
+StartInd=round(StartTime/TrekSet.tau)+1;
+EndInd=round(EndTime/TrekSet.tau)+1;
+
+trek=TrekSet.trek(StartInd:EndInd);
+NoiseSet=NoiseHist(trek);
+
+TrekSet.NoiseSet=NoiseSet;
+TrekSet.trek=TrekSet.trek-NoiseSet.MeanVal;
+TrekSet.MeanVal=0;
+TrekSet.MaxSignal=TrekSet.MaxSignal-NoiseSet.MeanVal;
+TrekSet.MinSignal=TrekSet.MinSignal-NoiseSet.MeanVal;
+TrekSet.Threshold=NoiseSet.Thr;
+TrekSet.StdVal=NoiseSet.StdVal;
+
+longD=trek-circshift(trek,TrekSet.STP.FrontN);
+NoiseSet=NoiseHist(longD);
+TrekSet.ThresholdLD=NoiseSet.Thr;
