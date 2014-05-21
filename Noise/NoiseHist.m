1d132091fb9179af422f8547d32116fa5c2745d5
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-05-21 19:13:30 +0400

    NoiseHist. New algorithm fitting throw Hist. Not completed

--- Noise/NoiseHist.m
+++ Noise/NoiseHist.m
@@ -43,7 +43,7 @@ while isempty(ch)
     NinCentralChan=max([100,NNoise/Nfit]); 
     dX=erfinv(NinCentralChan/A/sqrt(2*pi)/Std(end))*sqrt(2)*Std(end); %halfwidth of histogram chanel for 100 points in central channel
     dX=max([RangeNoise/Nfit,dX,minStep]);   % to avoid a lot of channels (=very small step)
-    dX=min([Std/2,dX]);                     % to avoid a few channels (= to narrow channel width)
+    dX=min([Std(end)/2,dX]);                     % to avoid a few channels (= too narrow channel width)
 
     HistLowBorder=(Median(end)-Std(end));
     HistHighBorder=(Median(end)+Std(end));
@@ -57,11 +57,22 @@ while isempty(ch)
         HistNoise=HistOnNet(trek(BoolNoise),[min(trek(BoolNoise)):2*dX:max(trek(BoolNoise))]);
         bool=HistNoise(:,1)>=HistLowBorder&HistNoise(:,1)<=HistHighBorder;
         i=numel(find(HistNoise(bool,2)<10));
-    end;  
+    end;
+   
+   [m,mi]=max(HistNoise(:,2));
+   if numel(find(bool))<3               % avoiding bad polyfit by power 2
+       bool=HistNoise(:,1)>=HistNoise(mi-1,1)&HistNoise(:,1)<=HistNoise(mi+1,1);
+   end;
+    
     
     ch1='';
+    i=0;
+    MaxDif=[];
+    MeanDif=[];
+    NfitHist=[];
     
     while isempty(ch1)
+        i=i+1;
         [fit,s,m]=polyfit(HistNoise(bool,1),log(HistNoise(bool,2)),2);
         mid=-fit(2)/(2*fit(1))*m(2)+m(1);
         r=roots(fit)*m(2)+m(1);
@@ -69,33 +80,70 @@ while isempty(ch)
         dif=abs(HistNoise(:,2)-exp(polyval(fit,HistNoise(:,1),s,m)))./HistNoise(:,2);
         dif(HistNoise(:,2)==0)=1; %avoiding inf
         
-        if all(dif<=1)
-            boolNew=dif<=max(dif(bool))|dif<=4*std(dif(bool));
-            boolNew(find(boolNew,1,'first'):find(boolNew,1,'last'))=true;
-            if numel(find(boolNew))==numel(find(bool))&&all(intersect(find(bool),find(boolNew))==find(bool)) 
-                 ch1='e'; 
-            else
-                 bool=boolNew;
-            end;
-            Mean=[Mean;mid];
-            Median=[Median;mid];
-            Std=[Std;stdev];
-            Thr=[Thr;mean(abs(r-mid))];
+        Mean=[Mean;mid];
+        Median=[Median;mid];
+        Std=[Std;stdev];
+        Thr=[Thr;mean(abs(r-mid))];        
+        
+        NfitHist(i)=numel(find(bool));
+        MaxDif(i)=max(dif(bool));
+        MeanDif(i)=mean(dif(bool));
+        
+        %Check condition for exit and if false pick new fitting histogram
+        %points
+        if i>2&&any(MaxDif<=1)&&any(stdev==Std(end-i+1:end-1))
+            Std(end)=[];
+            Thr(end)=[];
+            Mean(end)=[];
+            Median(end)=[];
+            NfitHist(end)=[];
+            MaxDif(end)=[];
+            MeanDif(end)=[];
+            i=i-1;
+            ch1='e';
         else
-            [m,mi]=max(HistNoise(:,2));
-            Mean=[Mean;HistNoise(mi,1)];
-            Median=[Median;HistNoise(mi,1)];
-            Std=[Std;-1];
-            Thr=[Thr;-1];
-            bool=HistNoise(:,1)>=HistNoise(mi-1,1)&HistNoise(:,1)<=HistNoise(mi+1,1);
+            if i==1&&NfitHist(end)>3
+                bool=HistNoise(:,1)>=HistNoise(mi-1,1)&HistNoise(:,1)<=HistNoise(mi+1,1);
+            else
+                if  all(dif<=1)&&NfitHist(end)==3
+                    bool=HistNoise(:,1)>=HistNoise(max([1,mi-2]),1)&HistNoise(:,1)<=HistNoise(min([mi+2,size(HistNoise,1)]),1);
+                end;
+                if ~all(dif<=1)
+                    bool=false(size(HistNoise,1),1);
+                    bool(min([find(dif>1,1,'first'),mi-1]):max([find(dif>1,1,'last'),mi+1]))=true;
+                    if numel(find(bool))<=3
+                        bool=HistNoise(:,1)>=HistNoise(mi-2,1)&HistNoise(:,1)<=HistNoise(mi+2,1);
+                    end;                    
+                end;
+                if all(dif<=1)&&NfitHist(end)>3
+                    bool=false(size(HistNoise,1),1);
+                    bool(find(dif<0.5,1,'first'):find(dif<0.5,1,'last'))=true;
+                    if numel(find(bool))<=3
+                        bool=HistNoise(:,1)>=HistNoise(mi-2,1)&HistNoise(:,1)<=HistNoise(mi+2,1);
+                    end;                    
+                end;
+            end;
         end;
-
-
     end;
+    % pick the best fit
+    
+    Ind=find(NfitHist>3&MaxDif<1);
+    if ~isempty(Ind)
+        [m,mi]=min(MeanDif(Ind));
+        Ind=Ind(mi);
+    else
+        [m,Ind]=min(MeanDif);        
+    end;
+       Std(end+1)=Std(end-i+Ind);
+       Thr(end+1)=Thr(end-i+Ind);
+       Mean(end+1)=Mean(end-i+Ind);
+       Median(end+1)=Median(end-i+Ind);
+       
+    
     
     BoolNoise=abs(trek-Median(end))<Thr(end);
     
-    if numel(find(bool))>5||numel(find(bool))>=fix(Std(end)/minStep) 
+    if numel(find(bool))>5||((numel(find(bool))>=fix(Std(end)/minStep))&&(dX<Std(end)/2))
         ch='1'; 
     end;   
 end;
@@ -107,3 +155,4 @@ NoiseSet.Thr=Thr(end);
 NoiseSet.NoiseInd=find(BoolNoise);
 NoiseSet.NoiseBool=BoolNoise;
 NoiseSet.MinStep=minStep;
+NoiseSet.HistStep=dX;
