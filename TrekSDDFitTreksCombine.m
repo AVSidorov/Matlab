aeb58290b1ddfbaad74ae1321804ed0e58bd9097
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-05-30 13:08:18 +0400

    TrekSDDFitTreksCombine. Modifications. Works (not fast enough)

--- TrekSDDFitTreksCombine.m
+++ TrekSDDFitTreksCombine.m
@@ -9,38 +9,50 @@ OverSt=3.5;
 
 TrekSet=TrekSetBase;
 Fit=TrekSDDFitTreks(TrekSetBase,TrekSet2);
+OverSt=mean([OverSt,Fit.DifNoise.Thr/Fit.DifNoise.StdVal]);
 boolBaseNotOverload=TrekSetBase.trek<TrekSetBase.MaxSignal&TrekSetBase.trek>TrekSetBase.MinSignal;
+P=PartsSearch(boolBaseNotOverload,TrekSet.STP.FrontN,TrekSet.STP.FrontN);
+boolBaseNotOverload=P.bool;
 
-bool=abs(Fit.dif)>Fit.DifNoise.Thr&Fit.boolNotOverload;
+TrekSet.trek(~boolBaseNotOverload)=TrekSet2.trek(~boolBaseNotOverload)*Fit.fit(1)+Fit.fit(2);
+TrekSet.MaxSignal=TrekSet2.MaxSignal*Fit.fit(1)+Fit.fit(2);
+TrekSet.MinSignal=TrekSet2.MinSignal*Fit.fit(1)+Fit.fit(2);
+
+trek=TrekSet.trek;
+difAll=trek-(TrekSet2.trek*Fit.fit(1)+Fit.fit(2));
+bool=abs(difAll)>OverSt*Fit.DifNoise.StdVal&Fit.boolNotOverload;
+
 PSetInitial=PartsSearch(bool,100);
 
 StartPlasmaInd=round((TrekSetBase.StartPlasma-TrekSetBase.StartTime)/TrekSetBase.tau);
-trek=TrekSetBase.trek;
+
 NextPartStart=StartPlasmaInd;
 NextPartEnd=PSetInitial.SpaceStart(find(PSetInitial.SpaceStart>StartPlasmaInd,1,'first')+1);
 
 while ~isempty(NextPartEnd)
     difAll=trek-(TrekSet2.trek*Fit.fit(1)+Fit.fit(2));
-    bool=abs(difAll)>Fit.DifNoise.Thr&Fit.boolNotOverload;
+    bool=abs(difAll)>OverSt*Fit.DifNoise.StdVal&Fit.boolNotOverload;
     PSet=PartsSearch(bool(NextPartStart:NextPartEnd),100);
     i=find(PSet.SpaceEnd+NextPartStart-1>=StartPlasmaInd,1,'first');
     Ind=NextPartStart-1+[PSet.SpaceEnd(i):PSet.SpaceStart(i)];
     if ~isempty(Ind)
-        ch=[];
-        while isempty(ch)
-            bool=false(size(trek));
-            bool(Ind)=true;       
-            dif=difAll(bool);
-            Polarity=sign(mean(dif));
-            bool=bool&Polarity*difAll>0&Fit.boolNotOverload;    
-            Ind=find(bool);
+        
+        bool=false(size(trek));
+        bool(Ind)=true;       
+        dif=difAll(bool);
+        Polarity=sign(mean(dif));
+        bool=bool&Polarity*difAll>0&Fit.boolNotOverload;    
+        Ind=find(bool);
+        if ~isempty(Ind)
             FitExp=polyfit([Ind-Ind(1)+1],log(Polarity*difAll(bool)),1);
             IndEnd=round(roots(FitExp))+Ind(1)-1;
-    %         Ind=Ind(1)-1+find((Polarity*Fit.dif(Ind(1):IndEnd)-exp(polyval(FitExp,[Ind(1):IndEnd]-Ind(1)+1))')<Fit.DifNoise.Thr);
-            ch=1;
+            %Ind=Ind(1)-1+find((Polarity*Fit.dif(Ind(1):IndEnd)-exp(polyval(FitExp,[Ind(1):IndEnd]-Ind(1)+1))')<Fit.DifNoise.Thr);
         end;
+        
         bool=false(size(trek));
-        bool(Ind(1):IndEnd)=true;
+        if ~isempty(Ind)
+            bool(Ind(1):IndEnd)=true;
+        end;
         bool=bool&boolBaseNotOverload;
         Ind=find(bool);
         if ~isempty(Ind)
@@ -48,6 +60,7 @@ while ~isempty(NextPartEnd)
             NextPartStart=Ind(1);
             NextPartEnd=PSetInitial.SpaceStart(find(PSetInitial.SpaceStart>Ind(end),1,'first'));   
         else
+            NextPartStart=NextPartStart+PSet.SpaceStart(i);
             NextPartEnd=PSetInitial.SpaceStart(find(PSetInitial.SpaceStart>NextPartEnd,1,'first'));
         end;
     else
@@ -56,6 +69,4 @@ while ~isempty(NextPartEnd)
 end;
 
 TrekSet.trek=trek;
-TrekSet.trek(~boolBaseNotOverload)=TrekSet2.trek(~boolBaseNotOverload)*Fit.fit(1)+Fit.fit(2);
-TrekSet.MaxSignal=TrekSet2.MaxSignal*Fit.fit(1)+Fit.fit(2);
-TrekSet.MinSignal=TrekSet2.MinSignal*Fit.fit(1)+Fit.fit(2);
+
