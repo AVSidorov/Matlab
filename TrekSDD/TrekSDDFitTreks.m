971a2c6d6e1fc9dcea58f5c102869df70d13deb4
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-06-05 19:34:45 +0400

    TrekSDDFitTreks. Added using StartPlasma field.

--- TrekSDD/TrekSDDFitTreks.m
+++ TrekSDD/TrekSDDFitTreks.m
@@ -9,17 +9,23 @@ boolSignal=boolNotOverload&...
 Ind=find(boolSignal);
 Ind1=[];
 ex=[];
+if isfield(TrekSet1,'StartPlasma')&&~isempty(TrekSet1.StartPlasma)&&round((TrekSet1.StartPlasma-TrekSet1.StartTime)/TrekSet1.tau)>100;
+    EndI=round((TrekSet1.StartPlasma-TrekSet1.StartTime)/TrekSet1.tau);
+else
+    EndI=TrekSet1.size;
+end;
 NoiseDif.MeanVal=0;
 NoiseDif.MedianVal=0;
 NoiseDif.StdVal=TrekSet1.StdVal;
 NoiseDif.Thr=TrekSet1.Threshold;
-NoiseDif.NoiseBool=boolSignal;
-NoiseDif.NoiseInd=find(boolSignal);
+NoiseDif.NoiseBool=true(EndI,1);
+NoiseDif.NoiseInd=NoiseDif.NoiseBool;
 NoiseDif.MinStep=TrekSet1.NoiseSet.MinStep;
 while isempty(ex)
     fit=polyfit(TrekSet2.trek(boolSignal),TrekSet1.trek(boolSignal),1);
     dif=TrekSet1.trek-TrekSet2.trek*fit(1)-fit(2);
-    NoiseDif=NoiseHist(dif,NoiseDif);
+    
+    NoiseDif=NoiseHist(dif(1:EndI),NoiseDif);
     boolSignal=boolSignal&abs(dif)<OverSt*NoiseDif.StdVal;
     Ind1=find(boolSignal);
     if numel(Ind)==numel(Ind1)&&numel(intersect(Ind,Ind1))==numel(Ind)
