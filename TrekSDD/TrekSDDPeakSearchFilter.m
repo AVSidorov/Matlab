0bf112bfd885daf86d6f2e9b977723b26f813033
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-10-09 14:23:54 +0300

    TrekSDDPeakSearchFilter. Start refactoring.

--- TrekSDD/TrekSDDPeakSearchFilter.m
+++ TrekSDD/TrekSDDPeakSearchFilter.m
@@ -1,11 +1,22 @@
 function [TrekSet,trek]=TrekSDDPeakSearchFilter(TrekSet,NoiseInd)
-kernel_type='gauss';
-kernel_width=10;
-kernel=load(['D:\!SCN\FilteringSDD\kernel_',kernel_type,'_',num2str(kernel_width),'.dat']);
-N=numel(abs(kernel(:,2))>0);
-Step=mean(diff(kernel(:,1)));
-kernel=interp1(kernel(:,1),kernel(:,2),[1:135]);
-kernel=kernel*1/Step;
+
+if ~isfield(TrekSet,'kernel')||isempty(TrekSet.kernel)
+    STP=TrekSet.STP;
+    time=(STP.TimeInd-1)*TrekSet.tau;
+    Stp(:,1)=time;
+    Stp(:,2)=STP.FinePulse;
+    [M,MI]=max(STP.FinePulse);
+    MaxTime=time(MI);
+    Resp(:,1)=Stp(:,1);
+    Resp(:,2)=exp(-(time-MaxTime).^2/(2*(sqrt(10)*0.02)^2))';
+    Kernel=MakeKernelByResponse(Resp,Stp,false);
+    kernel=KernelByTimeStep(Kernel,0.02);
+    TrekSet.kernel=kernel;
+end;
+
+kernel=TrekSet.kernel;
+kernel=kernel(1:137);
+
 trek=filter(kernel,1,TrekSet.trek);
 S=SpecialTreks(trek);
 NoiseSet=NoiseFitAuto(trek(NoiseInd));
