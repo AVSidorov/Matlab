fc203ce349d56b0283e17e4f5c1429d9c6b53604
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-10-17 14:58:50 +0300

    TrekSDDMerge. Repeated input is fixed.
    (cherry picked from commit ab6e5a5892ef0f3972ea32a72354375c5beecc88)

--- TrekSDD/TrekSDDMerge.m
+++ TrekSDD/TrekSDDMerge.m
@@ -32,7 +32,11 @@ for i=1:4
    if isempty(TrekSet)
        TrekSet=TrekSDDRecognize(FileName,varargin{:});
    else
-      TrekSet(end+1)=TrekSDDRecognize(FileName,varargin{:});
+      TrekSet(end+1)=TrekSDDRecognize(FileName,'Date',TrekSet(1).Date,... %was varagin{:} 
+                                               'Shot',TrekSet(1).Shot,... %if argin is empty, then repeated input follows
+                                               'Amp',TrekSet(1).Amp,...
+                                               'StartOffset',TrekSet(1).StartOffset,...
+                                               'StartPlasma',TrekSet(1).StartPlasma);
    end;
    TrekSet(end).Plot=false; 
    if TrekSet(end).type>0 
