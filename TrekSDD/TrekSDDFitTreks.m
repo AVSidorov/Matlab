110054ec3470d00d136c631919c468f7c2dd8b18
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-10-21 11:48:46 +0300

    TrekSDDFitTreks. Added part searching in Signal, noise and Overload determination.

--- TrekSDD/TrekSDDFitTreks.m
+++ TrekSDD/TrekSDDFitTreks.m
@@ -1,11 +1,20 @@
 function FitSet=TrekSDDFitTreks(TrekSet1,TrekSet2)
 
 OverSt=3.5;
+%% determination Overload, Signal, and Noise
 boolNotOverload=TrekSet1.trek>TrekSet1.MinSignal&TrekSet1.trek<TrekSet1.MaxSignal&...
                 TrekSet2.trek>TrekSet2.MinSignal&TrekSet2.trek<TrekSet2.MaxSignal;
 boolSignal=boolNotOverload&...
      abs(TrekSet1.trek)>TrekSet1.Threshold&abs(TrekSet2.trek)>TrekSet2.Threshold;
 
+Pset=PartsSearch(boolSignal,100,10); %to avoid adding to noise zero crossing points in signal
+                                     %TODO parts length automation
+boolSignal=Pset.bool;
+boolNoise=boolNotOverload&~boolSignal;
+Pset=PartsSearch(boolNoise,100,250);
+boolNoise=Pset.bool;
+
+%%
 Ind=find(boolSignal);
 Ind1=[];
 ex=[];
@@ -21,6 +30,7 @@ end;
 % NoiseDif.NoiseBool=true(EndI,1);
 % NoiseDif.NoiseInd=NoiseDif.NoiseBool;
 % NoiseDif.MinStep=TrekSet1.NoiseSet.MinStep;
+%% fitting
 while isempty(ex)
     fit=polyfit(TrekSet2.trek(boolSignal),TrekSet1.trek(boolSignal),1);
     dif=TrekSet1.trek-TrekSet2.trek*fit(1)-fit(2);
