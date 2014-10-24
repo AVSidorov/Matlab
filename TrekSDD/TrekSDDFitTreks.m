a45573d43814242b0fe75dad3b6c3f622d2489f6
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-10-24 15:12:53 +0300

    TrekSDDFitTreks. Added all boolean arrays to output structure.

--- TrekSDD/TrekSDDFitTreks.m
+++ TrekSDD/TrekSDDFitTreks.m
@@ -1,26 +1,39 @@
 function FitSet=TrekSDDFitTreks(TrekSet1,TrekSet2)
 
 OverSt=3.5;
-%% determination Overload, Signal, and Noise
+%% Determination Overload, Signal, Noise etc.
+% boolNotOverload - all points in ADC Range
+% boolOverload - points out of bound ADC Range and probably disturbed
+% boolDisturbed - points in ADC Range and probably disturbed
+% boolSignal - points whithin ADC range, isn't disturbed and above noise
+%               level
+% boolNoise=~boolSignal 
+
+%% determination Overload, Signal, Noise etc. 
 boolNotOverload=TrekSet1.trek>TrekSet1.MinSignal&TrekSet1.trek<TrekSet1.MaxSignal&...
                 TrekSet2.trek>TrekSet2.MinSignal&TrekSet2.trek<TrekSet2.MaxSignal;
+
 boolOverload=~boolNotOverload;            
-PSet=PartsSearch(boolOverload);
- % don't take parts after overloading (may be disturbed)
-    for i=1:numel(PSet.PartLength) 
-        boolOverload(PSet.SpaceStart(i)+1:PSet.SpaceStart(i)+TrekSet1.STP.size-TrekSet1.STP.MaxInd)=true;
-    end;
+PSet=PartsSearch(boolOverload,1,1);
+
+% search parts after overloading (may be disturbed)
+boolDisturbed = false(TrekSet1.size,1);
+for i=1:numel(PSet.PartLength) 
+    boolDisturbed(PSet.SpaceStart(i)+1:PSet.SpaceStart(i)+TrekSet1.STP.size-TrekSet1.STP.MaxInd)=true;
+end;
+
+boolOverload=~boolNotOverload|boolDisturbed;            
+
 % boolSignal=boolNotOverload&...
 
   boolSignal=~boolOverload&...            
       abs(TrekSet1.trek)>TrekSet1.Threshold&abs(TrekSet2.trek)>TrekSet2.Threshold;
 
-Pset=PartsSearch(boolSignal,100,10); %to avoid adding to noise zero crossing points in signal
+Pset=PartsSearch(boolSignal,100,1); %to avoid adding to noise zero crossing points in signal
                                      %TODO parts length automation
 boolSignal=Pset.bool;
 boolNoise=boolNotOverload&~boolSignal;
-Pset=PartsSearch(boolNoise,100,250);
-boolNoise=Pset.bool;
+
 
 %%
 Ind=find(boolSignal);
@@ -44,18 +57,21 @@ while isempty(ex)
     dif=TrekSet1.trek-TrekSet2.trek*fit(1)-fit(2);
     
     NoiseDif=NoiseStd(dif(boolSignal));
-    boolSignal=boolSignal&~boolNoise&abs(dif-NoiseDif.MeanVal)<NoiseDif.Thr;
+    boolSignal=boolSignal&abs(dif-NoiseDif.MeanVal)<NoiseDif.Thr;
     Ind1=find(boolSignal);
     if numel(Ind)==numel(Ind1)&&numel(intersect(Ind,Ind1))==numel(Ind)
         ex=1;
     end;
     Ind=Ind1;
 end;
-boolSignal=boolNotOverload&abs(dif)<NoiseDif.Thr;
 
 FitSet.fit=fit;
-FitSet.boolSignal=boolSignal;
+FitSet.boolSignal=boolSignal; % points where was fitting done
+FitSet.boolNoise=boolNoise;
 FitSet.boolNotOverload=boolNotOverload;
+FitSet.boolOverload=boolOverload;
+FitSet.boolDisturbed=boolDisturbed;
+
 
 FitSet.dif=dif;
 FitSet.DifNoise=NoiseDif;
