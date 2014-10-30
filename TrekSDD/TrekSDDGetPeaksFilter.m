45cd42bb4d4f8baeb0e2d139db58b5c8b8d1dce6
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-10-30 18:02:05 +0300

    Modification for precision peaks search. Works bad.

--- TrekSDD/TrekSDDGetPeaksFilter.m
+++ TrekSDD/TrekSDDGetPeaksFilter.m
@@ -1,38 +1,25 @@
 function TrekSet=TrekSDDGetPeaksFilter(TrekSet)
 BckgFitN=5;
 StartSearchTime=14500;
-NoiseTime=[2000,13000];
-NoiseInd=[fix((NoiseTime(1)-TrekSet.StartTime)/TrekSet.tau):fix((NoiseTime(end)-TrekSet.StartTime)/TrekSet.tau)];
-StartSearchInd=fix((StartSearchTime-TrekSet.StartTime)/TrekSet.tau);
-NoiseSet=NoiseFitAuto(TrekSet.trek(NoiseInd));
-% NoiseSet.MeanVal=median(TrekSet.trek(NoiseInd));
-% NoiseSet.StdVal=std(TrekSet.trek(NoiseInd));
-% NoiseSet.Threshold=5*NoiseSet.StdVal;
-TrekSet.trek=TrekSet.trek-NoiseSet.MeanVal;
-TrekSet.MeanVal=0;
-TrekSet.StdVal=NoiseSet.StdVal;
-TrekSet.MaxSignal=TrekSet.MaxSignal-NoiseSet.MeanVal;
-TrekSet.MinSignal=TrekSet.MinSignal-NoiseSet.MeanVal;
-TrekSet.Threshold=ceil(NoiseSet.Threshold);
-TrekSet.OverSt=3.5;
-
+TrekSetIn=TrekSet;
+TrekSet.peaks=[];
 STP=TrekSet.STP;
-[TrekSet,trek]=TrekSDDPeakSearchFilter(TrekSet,NoiseInd);
-TrekSet.SelectedPeakInd(TrekSet.SelectedPeakInd<StartSearchInd)=[];
-for i=1:numel(TrekSet.SelectedPeakInd-1)
+for i=1:size(TrekSetIn.peaks,1)
         FIT.Shift=0;
         FIT.FitPulse=TrekSet.STP.Stp;
         FIT.FitPulseN=TrekSet.STP.size;
-        FIT.MaxInd=TrekSet.SelectedPeakInd(i);
-        FIT.A=trek(TrekSet.SelectedPeakInd(i));
+        FIT.MaxInd=TrekSetIn.peaks(i,1);
+        FIT.A=TrekSetIn.peaks(i,5);
         FIT.B=0;
         FIT.Khi=inf;
-        Npoints=BckgFitN+TrekSet.SelectedPeakInd(i+1)-TrekSet.SelectedPeakInd(i);
+        Npoints=BckgFitN+TrekSetIn.peaks(i+1,1)-TrekSetIn.peaks(i,1);
         %not neccesary add FrontN because we add Front for first pulse, and
         %resiude front of next
-        Npoints=min([Npoints,STP.size-(STP.BckgFitN-BckgFitN)]);
+        Npoints=min([Npoints,STP.size-(STP.BckgFitN-BckgFitN),STP.FrontN+BckgFitN]); 
         %don't check for trek size, because work till previouse for last
         %marker
+        %last number is neccessary in case of lost peaks on tail. Filter
+        %noise is high :( 
         FIT.N=Npoints;
         FIT.FitIndPulseStrict=[STP.BckgFitN-BckgFitN+1:STP.BckgFitN-BckgFitN+Npoints]';
         if FIT.FitIndPulseStrict(end)<STP.MinFitPoint-BckgFitN
@@ -49,8 +36,8 @@ for i=1:numel(TrekSet.SelectedPeakInd-1)
         FIT=TrekSDDFitTime(TrekSet,FIT);
         [TrekSet,TrekSet1]=TrekSDDSubtract(TrekSet,FIT);
         [ExcelentFit,TrekSet,FIT1,Istart]=TrekSDDisGoodSubtract(TrekSet,TrekSet1,FIT,FIT);
-        if not(ExcelentFit)&&FIT.A>TrekSet.Threshold&&FIT.A<TrekSet.MaxSignal
-            TrekSet=TrekSet1;
-        end;
+%          if not(ExcelentFit)&&FIT.A>TrekSet.Threshold&&FIT.A<TrekSet.MaxSignal
+%              TrekSet1=TrekSet1;
+%          end;
         assignin('base','TrekSet',TrekSet);
 end;
