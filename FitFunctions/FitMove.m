4a55a02b0eb4f915b7109db38e21dd1995954775
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-11-07 18:54:38 +0300

    FitMove. Test function for use with fminbnd (buil-in Matlab function)

---
+++ FitFunctions/FitMove.m
@@ -0,0 +1,42 @@
+function [khi,FIT]=FitMove(Y,F,YInd,FInd,YBaseInd,FBaseInd,shift)
+cut='Y';
+FIT=[];
+khi=inf;
+
+N=min([numel(YInd),numel(FInd)]);
+
+%% indexes normalizing
+YIndNorm=YInd-YBaseInd;
+FIndNorm=FInd-FBaseInd;
+[Ind,iY,iF]=intersect(YIndNorm,FIndNorm);
+if numel(Ind)<N 
+    warning('Bad regions. Fit Indexes will be cutted.');
+    if strcmpi(cut,'Y')
+        YIndNorm=YIndNorm(iY);
+    else
+        FIndNorm=FIndNorm(iF);
+    end;
+end;
+%% shift range determination and check
+L1=FIndNorm(1)-YIndNorm(1);
+L2=YIndNorm(end)-FIndNorm(end);
+MaxShiftL=min([-L1,L2]);
+MaxShiftR=-min([L1,-L2]);
+
+%% shifting F
+if shift>MaxShiftR||shift<MaxShiftL
+    warning('Bad Shift');
+    return;
+end;
+Sh=fix(shift);
+sh=shift-fix(shift);
+%%% TODO Pulse generation function as parameter;
+Pulse=circshift(F,Sh);
+Pulse=interp1(1:numel(F),Pulse,[1:numel(F)]-sh,'linear',0)';
+
+FIndSh=FInd+Sh; % Fitting window is binded with F original form
+FIndShNorm=FIndSh-FBaseInd;
+Ind=intersect(YIndNorm,FIndShNorm);
+%% fitting
+
+[khi,FIT]=FitAB(Y(Ind+YBaseInd),Pulse(Ind+FBaseInd));
