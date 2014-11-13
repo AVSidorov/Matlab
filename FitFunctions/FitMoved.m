a705b139efcdf104e88246d46d2430c46a0d9228
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-11-13 17:07:58 +0300

    FitMoved is now wrapper for using various shift and fit functions.
    Shifting move to FitShift function

--- FitFunctions/FitMoved.m
+++ FitFunctions/FitMoved.m
@@ -1,51 +1,7 @@
-function [khi,FIT]=FitMoved(Y,F,YInd,FInd,YBaseInd,FBaseInd,shift)
-cut='Y';
+function [khi,FIT]=FitMoved(Y,F,YInd,FInd,YBaseInd,FBaseInd,shift,ShiftFcn,FitFcn)
+% Wrapper for using various shift and fit functions
 FIT=[];
 khi=inf;
-
-N=min([numel(YInd),numel(FInd)]);
-
-%% indexes normalizing
-
-YIndNorm=YInd-YBaseInd;
-FIndNorm=FInd-FBaseInd;
-[Ind,iY,iF]=intersect(YIndNorm,FIndNorm);
-if numel(Ind)<N 
-    warning('Bad regions. Fit Indexes will be cutted.');
-    if strcmpi(cut,'Y')
-        YIndNorm=YIndNorm(iY);
-    else
-        FIndNorm=FIndNorm(iF);
-    end;
-end;
-%% shift range determination and check
-
-L1=FIndNorm(1)-YIndNorm(1);
-L2=YIndNorm(end)-FIndNorm(end);
-MaxShiftL=min([-L1,L2]);
-MaxShiftR=-min([L1,-L2]);
-
-if shift>MaxShiftR||shift<MaxShiftL
-    warning('Bad Shift');
-    return;
-end;
-
-%% shifting F
-
-Sh=fix(shift);
-sh=shift-fix(shift);
-
-% Fitting window is binded with F original form
-% so we shift only not integer part and determine
-% new intersection (shift indexes);
-
-FIndShNorm=FInd-(FBaseInd-Sh);
-IndSh=intersect(YIndNorm,FIndShNorm);
-
-%%% TODO Pulse generation function as parameter;
-
-Pulse=interp1([1:numel(F)],F,IndSh+(FBaseInd-Sh)-sh,'linear','extrap')';
-
-
+[Yfit,Ffit]=ShiftFcn(Y,F,YInd,FInd,YBaseInd,FBaseInd,shift);
 %% fitting
-[khi,FIT]=FitAB(Y(IndSh+YBaseInd),Pulse);
+[khi,FIT]=FitFcn(Yfit,Ffit);
---
+++ FitFunctions/FitShift.m
@@ -0,0 +1,48 @@
+function [Y,F]=FitShift(Y,F,YInd,FInd,YBaseInd,FBaseInd,shift)
+%function for shifting f relative Y and choosing FitIndexes
+cut='Y';
+
+N=min([numel(YInd),numel(FInd)]);
+
+%% indexes normalizing
+
+YIndNorm=YInd-YBaseInd;
+FIndNorm=FInd-FBaseInd;
+[Ind,iY,iF]=intersect(YIndNorm,FIndNorm);
+if numel(Ind)<N 
+    warning('Bad regions. Fit Indexes will be cutted.');
+    if strcmpi(cut,'Y')
+        YIndNorm=YIndNorm(iY);
+    else
+        FIndNorm=FIndNorm(iF);
+    end;
+end;
+%% shift range determination and check
+
+L1=FIndNorm(1)-YIndNorm(1);
+L2=YIndNorm(end)-FIndNorm(end);
+MaxShiftL=min([-L1,L2]);
+MaxShiftR=-min([L1,-L2]);
+
+if shift>MaxShiftR||shift<MaxShiftL
+    warning('Bad Shift');
+    return;
+end;
+
+%% shifting F
+
+Sh=fix(shift);
+sh=shift-fix(shift);
+
+% Fitting window is binded with F original form
+% so we shift only not integer part and determine
+% new intersection (shift indexes);
+
+FIndShNorm=FInd-(FBaseInd-Sh);
+IndSh=intersect(YIndNorm,FIndShNorm);
+
+Pulse=interp1([1:numel(F)],F,IndSh+(FBaseInd-Sh)-sh,'linear','extrap')';
+
+%% output
+Y=Y(IndSh+YBaseInd);
+F=Pulse;
