b933f7f6e2568d105e689026c28cce94611c5908
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-11-17 00:52:21 +0300

    TrekSDD2FitShift. Removed checking of shift in case TrekSet fitting.

--- TrekSDD/TrekSDD2FitShift.m
+++ TrekSDD/TrekSDD2FitShift.m
@@ -5,6 +5,12 @@ function [Y,F,FIT]=TrekSDD2FitShift(Y,F,YInd,FInd,YBaseInd,FBaseInd,shift)
 YIndNorm=YInd-YBaseInd;
 FIndNorm=FInd-FBaseInd;
 [Ind,iY,iF]=intersect(YIndNorm,FIndNorm);
+%% Determination available shifts
+L1=FIndNorm(1)-YIndNorm(1);
+L2=YIndNorm(end)-FIndNorm(end);
+MaxShiftL=min([-L1,L2]);
+MaxShiftR=-min([L1,-L2]);
+
 %% shifting F
 
 Sh=fix(shift);
@@ -13,17 +19,22 @@ sh=shift-fix(shift);
 % Fitting window is binded with F original form
 % so we shift only not integer part and determine
 % new intersection (shift indexes);
-
 FIndShNorm=FInd-(FBaseInd-Sh);
 IndSh=intersect(YIndNorm,FIndShNorm);
 
 Pulse=TrekSDDGetFitPulse(F,-sh);
-
+%% expansion FitPulse
+if shift>MaxShiftR
+    Pulse=[zeros(Sh-MaxShiftR,1);Pulse];
+end;    
+if shift<MaxShiftL
+    Pulse=[Pulse;zeros(MaxShiftL-Sh,1)];
+end;    
 %% output
 FIT.MaxInd=YBaseInd;
 FIT.FitInd=IndSh+YBaseInd;
 FIT.FitIndPulse=IndSh+(FBaseInd-Sh);
-FIT.FitPulse=Pulse;
+FIT.FitPulse=circshift(Pulse,Sh);
 FIT.FitPulseN=numel(Pulse);
 
 Y=Y(IndSh+YBaseInd);
