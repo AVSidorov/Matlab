def5ea6784cf30246bb583c2bd860561c46539cd
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-11-17 16:39:32 +0300

    TrekSDD2FitShift. Use TrekSDDGetFitPulse with full shift

--- TrekSDD/TrekSDD2FitShift.m
+++ TrekSDD/TrekSDD2FitShift.m
@@ -5,38 +5,26 @@ function [Y,F,FIT]=TrekSDD2FitShift(Y,F,YInd,FInd,YBaseInd,FBaseInd,shift)
 YIndNorm=YInd-YBaseInd;
 FIndNorm=FInd-FBaseInd;
 [Ind,iY,iF]=intersect(YIndNorm,FIndNorm);
-%% Determination available shifts
-L1=FIndNorm(1)-YIndNorm(1);
-L2=YIndNorm(end)-FIndNorm(end);
-MaxShiftL=min([-L1,L2]);
-MaxShiftR=-min([L1,-L2]);
 
 %% shifting F
 
 Sh=fix(shift);
 sh=shift-fix(shift);
 
-% Fitting window is binded with F original form
-% so we shift only not integer part and determine
-% new intersection (shift indexes);
+% in case TrekSet IndSh is necessary for determenation used part of
+% original StandardPulse
 FIndShNorm=FInd-(FBaseInd-Sh);
 IndSh=intersect(YIndNorm,FIndShNorm);
 
-Pulse=TrekSDDGetFitPulse(F,-sh);
-%% expansion FitPulse
-if shift>MaxShiftR
-    Pulse=[zeros(Sh-MaxShiftR,1);Pulse];
-end;    
-if shift<MaxShiftL
-    Pulse=[Pulse;zeros(MaxShiftL-Sh,1)];
-end;    
+Pulse=TrekSDDGetFitPulse(F,-shift);
+
 %% output
 FIT.MaxInd=YBaseInd;
-FIT.FitInd=IndSh+YBaseInd;
-FIT.FitIndPulse=IndSh+(FBaseInd-Sh);
-FIT.FitPulse=circshift(Pulse,Sh);
+FIT.FitInd=Ind+YBaseInd; % window in trek is constant
+FIT.FitIndPulse=IndSh+(FBaseInd-Sh); % window in Stp moves
+FIT.FitPulse=Pulse;
 FIT.FitPulseN=numel(Pulse);
 
-Y=Y(IndSh+YBaseInd);
-F=Pulse(IndSh+(FBaseInd-Sh));
+Y=Y(Ind+YBaseInd);
+F=Pulse(Ind+FBaseInd);
 
