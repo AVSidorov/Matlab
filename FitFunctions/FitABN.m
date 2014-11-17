97edd60ddec4ee1eb64f99c36f716ede11bad0e0
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-11-17 21:14:58 +0300

    FitABN. Function for multidimensional  fit.

---
+++ FitFunctions/FitABN.m
@@ -0,0 +1,22 @@
+function [khi,FIT]=FitABN(Y,F)
+n=size(F,2);
+N=numel(Y);
+S=zeros(n);
+for i=1:n
+    for ii=1:n
+        S(i,ii)=sum(F(:,i).*F(:,ii));
+    end;
+end;
+for i=1:n
+    V(i,1)=sum(Y.*F(:,i));
+end;
+A=linsolve(S,V);
+
+for i=1:n
+    Ffit(:,i)=F(:,i)*A(i);
+end;
+khi=sum((Y-sum(Ffit,2)).^2)/N;
+FIT.A=A;
+FIT.khi=khi;
+FIT.Ffit=Ffit;
+FIT.Yfit=sum(Ffit,2);
