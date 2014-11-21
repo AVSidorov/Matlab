ca0504a03bb453ba37f9c2d6d4317cb24af6d611
Anton Sidorov <sidorov@mail.ioffe.ru>
2014-11-21 17:19:05 +0300

    TrekSDDGetFitIndDouble. Bug fixed

--- TrekSDD/TrekSDDGetFitIndDouble.m
+++ TrekSDD/TrekSDDGetFitIndDouble.m
@@ -2,7 +2,7 @@ function FitInd=TrekSDDGetFitIndDouble(TrekSet,TrekSet2)
 bool=(TrekSet2.trek~=TrekSet.trek);
 PSet=PartsSearch(bool,3,100);
 SubtractInd=find(bool);
-bool=TrekSet2.trek(bool)<TrekSet.StdVal*TrekSet.OverSt;
+bool=abs(TrekSet2.trek(bool))<TrekSet.StdVal*TrekSet.OverSt;
 PSet=PartsSearch(bool,3,TrekSet.STP.MinFitPoint-TrekSet.STP.BckgFitN+5);
 if numel(PSet.SpaceStart)>=1
     FitInd=[SubtractInd(1)-5:SubtractInd(1)+PSet.SpaceStart(1)-1]'; %-5 To avoid bad check after fiting and final subtraction
