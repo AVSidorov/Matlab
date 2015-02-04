73818a377f944c4a5a2675c73745756bda1af518
Anton Sidorov <sidorov@mail.ioffe.ru>
2015-02-04 16:27:55 +0300

    TrekSDDPeaksGenerator. Using PlasmaStart field is added.

--- TrekSDD/TrekSDDPeaksGenerator.m
+++ TrekSDD/TrekSDDPeaksGenerator.m
@@ -8,6 +8,10 @@ AmpNoise=0.0;
 AmpNoiseType='unif'; % see random function. Assumes using  'unif' and 'norm'
 TimeStepNoise=0.0;
 TimeStepType='unif';
+TimeStart=TrekSet.STP.size*TrekSet.tau;
+if isfield(TrekSet,'StartPlasma')&&~isempty(TrekSet.StartPlasma)
+    TimeStart=TimeStart+TrekSet.StartPlasma;
+end;
 
 nargsin=size(varargin,2);
 if ~isempty(varargin)&&mod(nargsin,2)~=0
@@ -18,12 +22,13 @@ end;
 for i=1:fix(nargsin/2) 
     eval([varargin{1+2*(i-1)},'=varargin{2*i};']);
 end;
+TrekSet.StartPlasma=TimeStart-TrekSet.STP.size*TrekSet.tau;
 TrekSet.peaks=zeros(N,7);
 
 if TimeStepType=='trek'
     t=random('unif',0,TimeStep*N,N,1);
     t=sortrows(t);
-    TrekSet.peaks(:,2)=TrekSet.STP.size*TrekSet.tau+t;
+    TrekSet.peaks(:,2)=TimeStart+t;
     TrekSet.peaks(:,5)=random('norm',Amp,AmpNoise,N,1);
 else   
     TrekSet.peaks(:,2)=TrekSet.STP.size*TrekSet.tau+[1:N]'*TimeStep+random(TimeStepType,0.0,TimeStepNoise,N,1);
