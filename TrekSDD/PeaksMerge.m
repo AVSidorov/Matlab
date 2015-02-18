650c973062e10267075c954856c69cae1fd9e266
Anton Sidorov <sidorov@mail.ioffe.ru>
2015-02-18 22:20:48 +0300

    PeaksMerge. Added saving history of mark

--- TrekSDD/PeaksMerge.m
+++ TrekSDD/PeaksMerge.m
@@ -6,11 +6,29 @@ if peaks2.Threshold>peaks1.Threshold
     peaks1=peaks;
     clear peaks;
 end;
+fwhm=[];
+if isfield(peaks2,'fwhm')&&~isempty(peaks2.fwhm)
+    fwhm=peaks2.fwhm;
+end;
+if isempty(fwhm)&&isfield(peaks2,'sigma')&&~isempty(peaks2.sigma)
+    fwhm=peaks2.sigma*2*sqrt(2*log(2))/0.02;
+end;
+
+%% remove old noise marks
 Ind=find(peaks2.trek(peaks1.Ind(:,1))<peaks2.Threshold);
 peaks1.Ind(Ind,:)=[];
+fprintf('removed %3.0f noise marks\n',numel(Ind));
+
+
+
+%% finding peaks to remove
+boolDoubled=false(peaks1.size,1);
+boolCombine=false(peaks1.size,1);
+boolDoubledDouble=false(peaks1.size,1);
 
 
 bool=false(peaks1.size,1);
+
 bool1=bool;
 bool2=bool;
 
@@ -18,28 +36,64 @@ bool1(peaks1.Ind(:,1))=true;
 bool2(peaks2.Ind)=true;
 
 bool=bool1|bool2;
-boolAlt=bool;
-% remove peaks with small distance (different markers for same pulse)
-Ind=find(bool);
-dInd=diff(Ind);
-IndDoubled=sort([Ind(dInd<=space);Ind(dInd<=space)+dInd(dInd<=space)]);
-boolDoubled=false(peaks1.size,1);
-boolDoubled(IndDoubled)=true;
-bool(boolDoubled&bool2)=false;
-boolAlt(boolDoubled&bool1)=false; % to save history of marker moving 
 
-origin=zeros(peaks1.size,1);
-origin(bool&bool2)=2;
-origin(bool&bool1)=1;
-origin(bool&(boolDoubled|(bool1&bool2)))=3;
 Ind=find(bool);
 IndL=circshift(Ind,-1);
 IndR=circshift(Ind,1);
-boolCombine=origin(Ind)==2&origin(IndL)==1&origin(IndR)==1&peaks2.trek(Ind)>peaks2.trek(IndL)&peaks2.trek(IndR);
-IndCombine=Ind(boolCombine);
-bool(IndCombine)=false;
-boolAlt(IndCombine)=false;
-boolFinal1=bool;
+
+origin=zeros(peaks1.size,1);
+origin(bool1)=origin(bool1)+1;
+origin(bool2)=origin(bool2)+2;
+
+
+
+% remove peaks with small distance (different markers for same pulse)
+
+dAfter=Ind(1:end-1)-Ind(2:end);
+dBefore=Ind(2:end)-Ind(1:end-1);
+dAfter=[dAfter;peaks1.size-Ind(end)];
+dBefore=[Ind(1);dBefore];
+dAfter=abs(dAfter);
+IndDoubled=Ind((origin(Ind)==2)&((origin(IndL)==1&dAfter<=space)|(origin(IndR)==1&dBefore<=space)));
+boolDoubled(IndDoubled)=true;
+
+IndDoubledDouble=Ind(origin(Ind)==2&origin(IndL)==1&origin(IndR)==1&dAfter<=space&dBefore<=space);
+boolDoubledDouble(IndDoubledDouble)=true;
+
+% find combined marks
+IndCombine=Ind(origin(Ind)==2&origin(IndL)==1&origin(IndR)==1&(IndL-IndR)<2*fwhm);
+boolCombine(IndCombine)=true;
+
+
+IndRemove=find(boolDoubled|boolCombine);
+IndPairL=find((origin(Ind)==1)&(origin(IndR)==2&dBefore<=space)); %indexes in space(size) Ind
+IndPairR=find((origin(Ind)==1)&(origin(IndL)==2&dAfter<=space));
+Ind(:,2)=Ind;
+Ind(IndPairL,2)=IndR(IndPairL);
+Ind(IndPairR,2)=IndL(IndPairR);
+
+[C,ia,ib]=intersect(Ind(:,1),IndRemove);
+Ind(ia,:)=[];
+
+
+
+[C,ia,ib]=intersect(peaks1.Ind(:,1),Ind(:,1));
+IndFinal=peaks1.Ind(ia,:);
+StepMarker=peaks1.StepMarker(ia);
+IndFinal(:,end+1)=Ind(ib,2);
+[C,ia]=setdiff(Ind(:,1),IndFinal(:,1));
+IndAdd=zeros(numel(ia),size(IndFinal,2));
+IndAdd(:,1)=Ind(ia,1);
+IndAdd(:,end)=Ind(ia,2);
+IndFinal=[IndFinal;IndAdd];
+StepMarker=[StepMarker;ones(numel(ia),1)*peaks2.step];
+[IndFinal,ia]=sortrows(IndFinal);
+StepMarker=StepMarker(ia);
+
+for i=2:size(IndFinal,2)
+   idx=find(IndFinal(:,i)==0);
+   IndFinal(idx,i)=IndFinal(idx,i-1);
+end;
 
 
 % bool=bool1;
@@ -58,11 +112,6 @@ boolFinal1=bool;
 % 
 % bool=xor(boolFinal1,boolFinal2);
 
-
 peaks=peaks2;
-% peaks.Ind=peaks1.Ind;
-peaks.Ind=find(boolFinal1);
-% peaks.Ind(:,end+1)=find(boolAlt);
-peaks.StepMarker=peaks1.StepMarker;
-peaks.StepMarker(~boolFinal1)=0;
-peaks.StepMarker(boolFinal1&~bool1)=peaks2.step;
+peaks.Ind=IndFinal;
+peaks.StepMarker=StepMarker;
