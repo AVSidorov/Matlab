126e5ee31e2c0ccf9a43acea170da510597961de
Anton Sidorov <sidorov@mail.ioffe.ru>
2016-03-23 14:40:00 +0300

    GaussResponse. Added symmetrization of pulse relative time points

--- TrekSDD/GaussResponse.m
+++ TrekSDD/GaussResponse.m
@@ -5,8 +5,12 @@ bool=InputPulse(:,2)>0;
 Ind=find(bool);
 [M,MI]=max(InputPulse(:,2));
 timeMax=InputPulse(MI,1);
+timeStep=mean(diff(InputPulse(:,1)));
+
 timeMaxResp=InputPulse(Ind(1),1)+sqrt(-2*sigma^2*log(InputPulse(Ind(1),2)))+sigma;
 timeMax=max([timeMax,timeMaxResp]);
+timeMax=round(timeMax/timeStep)*timeStep;
+
 Response=InputPulse;
 Response(:,2)=exp(-(Response(:,1)-timeMax).^2/(2*sigma^2));
 Response(1:Ind(1)-1,2)=0;
