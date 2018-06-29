4b2992b5c953ebd5e7c01bcf8e795c2103b30925
Anton Sidorov <sidorov@mail.ioffe.ru>
2018-06-29 13:02:32 +0300

    elm_read_3D. Reducing first column with grid point index is added.

--- ElmFire/elm_read_3D.m
+++ ElmFire/elm_read_3D.m
@@ -51,4 +51,25 @@ while ~feof(fid)
     clear tmp;        
 end
 fclose(fid);
+
+%% Reducing first column with grid point index if it exists
+%TODO make this checking before writing whole file.
+
+Nchk=5*10*100*1000; 
+    %it's estimation for five time blocks. The exact values could be taken
+    %from input files, but this requires additional arguments in the function input,
+    %that can be inconvenient in use
+chk=MatFile.tbl(1:Nchk,1);
+
+
+if all(chk-fix(chk)==0)&&...% check first column values are integer  
+    all(diff(chk)==1|diff(chk)==-range(chk))% check step is unity or jump from max value to 1 (min value)
+    %check periodicity    
+    acf=autocorr(chk,Nchk-1);
+    [pks,locs]=findpeaks(acf);
+      if all(diff(locs)==range(chk)+1)
+          tic; MatFile.tbl=MatFile.tbl(:,2:end); toc;
+          MatFile.Ngrid=range(chk)+1;
+      end;
+end
 toc(MainTimer);
