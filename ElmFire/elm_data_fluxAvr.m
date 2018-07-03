e63c4ca32b30c7fcd19ca102aa4d3a75a859745a
Anton Sidorov <sidorov@mail.ioffe.ru>
2018-07-03 13:16:25 +0300

    SMALLFIX. elm_data_fluxAvr. Comment changed. TODO added

--- ElmFire/elm_data_fluxAvr.m
+++ ElmFire/elm_data_fluxAvr.m
@@ -2,6 +2,7 @@ function tblAv=elm_data_fluxAvr(tbl,GridSet)
 % flux surface averaging
 %TODO cheking size of input array
 tblAv=tbl;
+%TODO grid volume weighting
 for i=1:GridSet.dnx  
         tblAv(GridSet.SurfaceInd(i,1):GridSet.SurfaceInd(i,2),:)=repmat(mean(tblAv(GridSet.SurfaceInd(i,1):GridSet.SurfaceInd(i,2),:)),GridSet.Grid.Npoloidal(i),1);
 end;
