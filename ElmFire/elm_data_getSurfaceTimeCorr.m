05db3b7d580de998a3d30e64685642514a99e870
Anton Sidorov <sidorov@mail.ioffe.ru>
2018-07-04 15:43:42 +0300

    NEW. Functions for getting  time cross-correlation of fluctuation for flux surfaces and "correlation time metrics".

---
+++ ElmFire/elm_data_getSurfaceTimeCorr.m
@@ -0,0 +1,35 @@
+function [MaxCorrTab,LagCorrTab]=elm_data_getSurfaceTimeCorr(dtbl)
+% This function gets time cross-correlation of fluctuation for flux suface
+% dtbl is three dimensional poloidal x toroidal x time 
+% relative fluctuations table
+% MaxCorrTab is maximum of crosscorrelation between time layers
+% LagCorrTab is lag of maximum of crosscorrelation between time layers
+dny=size(dtbl,1);
+dnz=size(dtbl,2);
+dnt=size(dtbl,3);
+maxLags=dny*2-1;
+NmaxDeterm=3;
+d=fix(NmaxDeterm/2);
+LagCorrTab=zeros(dnt,dnt,dnz);
+MaxCorrTab=LagCorrTab;
+% nz=1;
+ for nz=1:dnz
+    tic;
+     for i=1:dnt
+        %matrix will by symmetric/antisymmetric 
+        %so loop starts from i+1 to reduce number of cycles and don't calculate autocorrelation        
+        for ii=i+1:dnt 
+            [CrossCorr,lags]=xcorr(dtbl(:,nz,i),dtbl(:,nz,ii),maxLags,'coeff');
+            [m,mi]=max(CrossCorr);
+            fitInd=max([1,mi-d]):min([length(CrossCorr),mi+d]);
+            if numel(fitInd)>2
+                [fit,s,mu]=polyfit(lags(fitInd)',CrossCorr(fitInd),2);
+                LagCorrTab(i,ii,nz)=((-fit(2)/2/fit(1))*mu(2)+mu(1));
+                MaxCorrTab(i,ii,nz)=polyval(fit,LagCorrTab(i,ii,nz),s,mu);
+            end;
+        end
+     end
+    toc;
+    MaxCorrTab(:,:,nz)=MaxCorrTab(:,:,nz)+MaxCorrTab(:,:,nz)'+eye(dnt);
+    LagCorrTab(:,:,nz)=LagCorrTab(:,:,nz)-LagCorrTab(:,:,nz)';
+ end
