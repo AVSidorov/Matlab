9d5cec5dba105a2f2ce22b37ec80d4b9f1de78a8
Anton Sidorov <sidorov@mail.ioffe.ru>
2018-07-13 12:54:42 +0300

    NEW. Functions for getting velocity from density.

--- ElmFire/elm_data_getRotationSpeed.m
+++ ElmFire/elm_data_getRotationSpeed.m
@@ -1,24 +1,42 @@
-function corrLag=elm_data_getRotationSpeed(dtbl)
+function [corrLag,corrVal]=elm_data_getRotationSpeed(dtbl,timeShiftMax)
 % This function extracts plasma  fluctuations poloidal rotation speed
 % dtbl is three dimensional poloidal x toroidal x time 
 % relative fluctuations table
 % corrLag is Lag in poloidal dimension to get max correlation scaled to one time step
 % table format is timeShift x toroidal x time
 % timeShift is time difference between time layers used for corrLag extraction
-corrLag=zeros(5,8,size(dtbl,3)-1);
-NmaxDeterm=9;
+if nargin<2
+    timeShiftMax=15;
+end
+
+dny=size(dtbl,1);
+dnz=size(dtbl,2);
+dnt=size(dtbl,3);
+
+corrLag=zeros(timeShiftMax,dnz,dnt-1);
+corrVal=corrLag;
+
+NmaxDeterm=3;
 d=fix(NmaxDeterm/2);
-maxLags=50;
-for timeShift=[1:15];
-    for nZ=1:8
-        for i=1:size(dtbl,3)-timeShift
-            [CrossCorr(:,i),lags]=xcorr(dtbl(:,nZ,i),dtbl(:,nZ,i+timeShift),maxLags);
+maxLags=150;
+
+for timeShift=1:timeShiftMax
+    for nZ=1:dnz
+        for i=1:dnt-timeShift
+            [CrossCorr(:,i),lags]=xcorr(dtbl(:,nZ,i),dtbl(:,nZ,i+timeShift),maxLags,'coeff');
         end
         [m,mi]=max(CrossCorr);
         % searchig fine max by parabolic fit
         for i=1:size(dtbl,3)-timeShift
-            [fit,s,mu]=polyfit(lags(mi(i)-d:mi(i)+d)',CrossCorr(mi(i)-d:mi(i)+d,i),2);
-            corrLag(timeShift,nZ,i)=((-fit(2)/2/fit(1))*mu(2)+mu(1))/timeShift;
-        end;
+            FitInd=max([1,mi(i)-d]):min([mi(i)+d,2*maxLags+1]);
+            [fit,s,mu]=polyfit(lags(FitInd)',CrossCorr(FitInd,i),2);
+            if ~isempty(fit)
+                corrLag(timeShift,nZ,i)=((-fit(2)/2/fit(1))*mu(2)+mu(1))/timeShift;
+                corrVal(timeShift,nZ,i)=polyval(fit,corrLag(timeShift,nZ,i),s,mu);
+            else
+                corrLag(timeShift,nZ,i)=mi(i);
+                corrVal(timeShift,nZ,i)=m(i);
+            end;
+        end;            
     end;
 end
