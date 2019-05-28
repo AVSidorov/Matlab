d1b769798fc44a9d3bf8d44560995c380b134383
Anton Sidorov <sidorov@mail.ioffe.ru>
2019-05-28 17:21:47 +0300

    DensityTrekLoadFromSingle added multiple file choice and loading

--- Density/DensityTrekLoadFromSingle.m
+++ Density/DensityTrekLoadFromSingle.m
@@ -1,13 +1,26 @@
-function [trek,FileName,PathStr]=DensityTrekLoadFromSingle
+function [trek,FileName,PathName]=DensityTrekLoadFromSingle
 
-[FileName,PathName,FilterIndex]=uigetfile('*.dat','Pick .dat file of single with video signal');
+[FileName,PathName,FilterIndex]=uigetfile('*.dat','Pick .dat file of single with video signal','MultiSelect','on');
 trek=[];
-if isstr(FileName)
-    fid=fopen(fullfile(PathName,FileName),'r');
-    trek=fread(fid,inf,'single');
-    fclose(fid);
-    trek(end-8:end)=[];
-    trek=trek-mean(trek);
+if isnumeric(FileName)&&FileName==0       
+    return;
 end;
+
+if ischar(FileName)
+    FileName=cellstr(FileName);
+end;
+
+for i=1:numel(FileName)
+    fid=fopen(fullfile(PathName,FileName{i}),'r');
+    trek(:,i)=fread(fid,inf,'single');
+    fclose(fid);    
+    trek(:,i)=trek(:,i)-mean(trek(1:end-8,i));
+end;
+trek(end-7:end,:)=[];
+%make length trek even
+if mod(length(trek),2)==1
+    trek(end,:)=[];
+end;
+
     
     
