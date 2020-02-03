5e93a5ed9b6569b9f34fca19513a8fedb55c36aa
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-02-03 17:55:44 +0300

    density_from_phase refactored for using already smoothed phase

--- Density/density_from_phase.m
+++ Density/density_from_phase.m
@@ -1,22 +1,29 @@
-function [rxy,denL,denR]=density_from_phase(phase,rxy0)
+function [rxy,denL,denR]=density_from_phase(xx,phase_smth,rxy0)
 %phase smoothing and intepolation
 rlim=0.078;
-rmax=0.08;
-phase_bck=0.01;
-xx=[-rmax:0.001:rmax]';
-phase_smth=csaps([-rmax;-rlim;phase(:,1); rlim; rmax ],[0;phase_bck;phase(:,2);phase_bck;0],1,xx);
+rmax=0.085;
+
+% Smoothing must be separated function
+% xx=[-rmax:0.001:rmax]';
+% phase_smth=csaps([-rmax;-rlim;phase(:,1); rlim; rmax ],[0;phase_bck;phase(:,2);phase_bck;0],1,xx);
 
 %intial rxy
-if nargin<2||isempty(rxy0)
+if nargin<3||isempty(rxy0)
     rxy=density_rcn_from_XN([xx,phase_smth]);
 else
     rxy=rxy0;
 end;
-rxy(:,3)=0;
-rxy=rxy(isfinite(rxy(:,1))&isfinite(rxy(:,2))&isfinite(rxy(:,3)),:);
-rxy(rxy(:,1)>=rlim,:)=[];
-rxy(end+1,:)=[rlim 0 0];
-rxy(end+1,:)=[rmax 0 0];
+
+% Preparing rxy must be whiht phase smoothing
+% %prepare rxy
+% rxy(:,3)=0;
+% rxy=rxy(isfinite(rxy(:,1))&isfinite(rxy(:,2))&isfinite(rxy(:,3)),:);
+% rxy(rxy(:,1)+sqrt(rxy(:,2).^2+rxy(:,3).^2)>=rlim,:)=[]; %% all surfaces must be inside dia
+% rxy=sortrows(rxy);
+% 
+% rxy(end+1,:)=[rlim 0 0];
+% rxy(end+1,:)=[rmax 0 0];
+
 rxy=sortrows(rxy);
 
 
@@ -36,8 +43,8 @@ function [khi,denL,denR]=calc_den(d)
     %prepare matrix and chords
     [AL,AR,xChordL,xChordR]=density_chords4solving(rxy_work(n:end,:));
     %interpolate phase in chords points
-    phase_smthL=interp1(xx,phase_smth,xChordL,'PChip');
-    phase_smthR=interp1(xx,phase_smth,xChordR,'PChip');
+    phase_smthL=interp1(xx,phase_smth,xChordL,'PChip',0);
+    phase_smthR=interp1(xx,phase_smth,xChordR,'PChip',0);
     %finding densities
     denL=linsolve(AL,phase_smthL);
     denR=linsolve(AR,phase_smthR);
