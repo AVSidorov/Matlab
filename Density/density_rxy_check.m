098b56c715ae209398566030b1ba9afc8d99c7ba
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-02-03 17:54:11 +0300

    density_rxy_check Added bool output for analyzing errors.

--- Density/density_rxy_check.m
+++ Density/density_rxy_check.m
@@ -1,4 +1,5 @@
-function ok=density_rxy_check(rxy)
+function [ok,bool]=density_rxy_check(rxy)
 rxy=rxy(isfinite(rxy(:,1))&isfinite(rxy(:,2))&isfinite(rxy(:,3)),:);
 deltaCentr=sqrt(diff(rxy(:,2)).^2+diff(rxy(:,3)).^2);
-ok=all(diff(rxy(:,1))>deltaCentr);
+bool=diff(rxy(:,1))>deltaCentr;
+ok=all(bool);
