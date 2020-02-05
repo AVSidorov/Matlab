312368776febb0f926985966d32df356b59cea2d
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-02-05 14:42:34 +0300

    density_raytrace_By_rxyV initial. Works

---
+++ Density/density_raytrace_By_rxyV.m
@@ -0,0 +1,133 @@
+function XYRV=density_raytrace_By_rxyV(slope,intercpt,rxy,N)
+%% const in CGS
+LAMBDA         = 0.22;%2.15E-1;%wavelength of the interferometer
+ELECTRONCHARGE = 4.8027E-10;
+LIGHTVELOS     = 2.9979E10; 
+ELECTRONMASS   = 9.1083E-28;
+OMEGA          = 2*pi*LIGHTVELOS/LAMBDA;
+DENSCOEFF      = (ELECTRONCHARGE)^2/OMEGA/LIGHTVELOS/ELECTRONMASS;%*2*pi;
+OMEGAPLCOEFF   = 4*pi*ELECTRONCHARGE^2/ELECTRONMASS;
+%% data prepare
+%in case of refraction slope is intial value
+if nargin<4&&size(rxy,2)==4
+    N=rxy(:,4);
+elseif nargin<4||isempty(N)
+    N=ones(size(rxy,1),1);
+end
+bool=rxy(:,1)<=0|isnan(rxy(:,1));
+rxy(bool,:)=[];
+N(bool,:)=[];
+% to avoid null division
+N(N==0)=eps;
+lenRXY=size(rxy,1);
+XYRV=zeros(lenRXY*2,4);
+
+%% convert dencity to refractive index
+Den=N;
+if max(N)<1e10 %convert phase to density unit
+    N=N/(DENSCOEFF*100); %*100 because working in meters
+end;
+N=sqrt(1-N*OMEGAPLCOEFF/OMEGA^2);    
+
+%% trace 
+
+% we shall start from max radius
+
+[rxy,index]=sortrows(rxy,-1);
+N=N(index);
+
+%propagation direction must be traced because reflection is possible
+drc=-1; %from top to bottom
+
+currentR=1; 
+currentPoint=1;
+
+trace(true); %trace inside
+trace(false); %trace outside
+
+
+function curR=trace(InOut)
+    stI=currentR;
+    if InOut
+        stp=1;
+        endI=lenRXY;
+    else
+        stp=-1;
+        endI=1;
+    end
+    for curR=stI:stp:endI
+        %% find intersections
+        [xi,yi]=linecirc(slope,intercpt,rxy(curR,2),rxy(curR,3),rxy(curR,1));
+        %exit if no intersection
+        if ~all(isfinite([xi,yi]))
+            currentR=curR-1;
+            break;
+        end
+        
+        %% pick refraction point from intersections
+        if InOut
+            %leave only upper part
+            [yi,ind]=max(yi);
+        else
+            %leave only lower part
+            [yi,ind]=min(yi);
+        end
+        xi=xi(ind);    
+        XYRV(currentPoint,1)=xi;
+        XYRV(currentPoint,2)=yi;    
+        XYRV(currentPoint,3)=rxy(curR,1);
+        XYRV(currentPoint,4)=Den(curR);
+        currentPoint=currentPoint+1;
+
+        %% refraction
+        if curR~=1&&curR~=endI % for inner/outter circle no refraction           
+            %% changing ray vector
+
+            %incedence ray vector (before)
+%             if currentPoint<=2
+%                 l=[cos(atan(slope)) sin(atan(slope))];
+%             else
+                l=[xi-XYRV(currentPoint-2,1) yi-XYRV(currentPoint-2,2)];
+                l=l/norm(l);
+            % this way of l determination is better so as vectors l=-l have same slope
+            %TODO add artificially start point of ray to use second equation in all cases
+%             end
+            %v1=[cos(atan(slope)) sin(atan(slope))]*V(i);
+
+            % normalized vector from center is normal to circle density border
+            n=[xi-rxy(curR,2) yi-rxy(curR,3)];
+            n=stp*n/norm(n); %unit vector stp=1 means propagation inside circle stp=-1 outside
+
+            if InOut
+                R=N(curR-stp)/N(curR);    %density inside ring is equal to one on it
+            else
+                R=N(curR)/N(curR+stp);
+            end
+            C=-n*l';
+
+            %refracted (new) ray vector
+            v=R*l+(R*C-sqrt(1-R^2*(1-C^2)))*n; %wiki eng
+            %v2=v1+(sqrt((V(i+1)^2+V(i)^2)/(v1*r')^2+1)-1)*(v1*r')*r;
+
+            if ~isreal(v) %reflection
+                drc=1;
+                v=l-2*(l*n')*n;
+                break;
+            end;
+            %% new line for intersections
+            %new slope and intercpt
+            %if v(1)~=0
+            if abs(v(1))>eps
+                slope=v(2)/v(1);
+                intercpt=yi-xi/v(1)*v(2);
+            else
+                slope=inf*sign(v(2));
+                intercpt=xi;
+            end;
+        elseif curR==1
+            XYRV=XYRV(1:currentPoint-1,:);
+        end
+    end
+end
+
+end
