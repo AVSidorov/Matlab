734751f4dd69b3ef82b56dcf1cf5ad03bddb498b
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-03-10 16:27:17 +0300

    density_xy2r. Fixed determination r outsid rxyn range

--- Density/density_xy2r.m
+++ Density/density_xy2r.m
@@ -1,3 +1,12 @@
 function r=density_xy2r(x,y,rxy)
 dist2Centr=sqrt((rxy(:,2)-x).^2+(rxy(:,3)-y).^2);
-r=interp1(rxy(:,1)-dist2Centr,rxy(:,1),0);
+[~,maxRind]=max(rxy(:,1));
+[~,minRind]=min(rxy(:,1));
+if all(dist2Centr-rxy(:,1)>0)
+    r=sqrt((rxy(maxRind,2)-x).^2+(rxy(maxRind,3)-y).^2);
+elseif all(dist2Centr-rxy(:,1)<0)
+    r=sqrt((rxy(minRind,2)-x).^2+(rxy(minRind,3)-y).^2);
+else
+    r=interp1(rxy(:,1)-dist2Centr,rxy(:,1),0);
+    
+end
