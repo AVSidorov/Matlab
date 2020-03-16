d9ad915e62000e77bcf3e34860d48e0d1758337f
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-03-16 13:33:44 +0300

    density_rxy_check. fixed for sortded in descending by r rxyn

--- Density/density_rxy_check.m
+++ Density/density_rxy_check.m
@@ -1,5 +1,5 @@
 function [ok,bool]=density_rxy_check(rxy)
 rxy=rxy(isfinite(rxy(:,1))&isfinite(rxy(:,2))&isfinite(rxy(:,3)),:);
 deltaCentr=sqrt(diff(rxy(:,2)).^2+diff(rxy(:,3)).^2);
-bool=diff(rxy(:,1))>deltaCentr;
+bool=abs(diff(rxy(:,1)))>deltaCentr;
 ok=all(bool);
