ae2feecf5c8c95504b33097b6956467b1e3abbc5
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-05-05 15:45:58 +0300

    density_xy2r refactor so output same size as input

--- Density/density_xy2r.m
+++ Density/density_xy2r.m
@@ -1,12 +1,18 @@
 function r=density_xy2r(x,y,rxy)
-dist2Centr=sqrt((rxy(:,2)-x).^2+(rxy(:,3)-y).^2);
-[~,maxRind]=max(rxy(:,1));
-[~,minRind]=min(rxy(:,1));
-if all(dist2Centr-rxy(:,1)>0)
-    r=sqrt((rxy(maxRind,2)-x).^2+(rxy(maxRind,3)-y).^2);
-elseif all(dist2Centr-rxy(:,1)<0)
-    r=sqrt((rxy(minRind,2)-x).^2+(rxy(minRind,3)-y).^2);
-else
-    r=interp1(rxy(:,1)-dist2Centr,rxy(:,1),0);
-    
+if any(abs(size(x)-size(y))>eps)
+    error('x and y must have same size');
+end;
+r=zeros(size(x));
+for i=1:numel(x)
+    dist2Centr=sqrt((rxy(:,2)-x(i)).^2+(rxy(:,3)-y(i)).^2);
+    [~,maxRind]=max(rxy(:,1));
+    [~,minRind]=min(rxy(:,1));
+    if all(dist2Centr-rxy(:,1)>0)
+        r(i)=sqrt((rxy(maxRind,2)-x(i)).^2+(rxy(maxRind,3)-y(i)).^2);
+    elseif all(dist2Centr-rxy(:,1)<0)
+        r(i)=sqrt((rxy(minRind,2)-x(i)).^2+(rxy(minRind,3)-y(i)).^2);
+    else
+        r(i)=interp1(rxy(:,1)-dist2Centr,rxy(:,1),0);
+
+    end
 end
