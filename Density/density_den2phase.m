55da45a8a4d278e634cff004742d542d68003dcf
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-05-05 15:44:46 +0300

    density to phase/phase to density  now exact
    freq as input parameter

--- Density/density_den2phase.m
+++ Density/density_den2phase.m
@@ -1,12 +1,21 @@
-function ph=density_den2phase(den)
+function ph=density_den2phase(den,freq)
+% ph is phase addition per length unit  dPhase[rad/2/pi]/dL [meters] 
 %% const in CGS
 ELECTRONCHARGE = 4.8027E-10;
 LIGHTVELOS     = 2.9979E10; 
-ELECTRONMASS   = 9.1083E-28;
+LIGHTVELOS     = 299792458*100;
+ELECTRONMASS   = 9.10938356E-28;
 
-LAMBDA         = 0.22;%2.15E-1;%wavelength of the interferometer
-OMEGA          = 2*pi*LIGHTVELOS/LAMBDA;
-OMEGA=2*pi*136e9; %
+if nargin<2||isempty(freq)
+    LAMBDA         = 0.22;%2.15E-1;%wavelength of the interferometer
+    OMEGA          = 2*pi*LIGHTVELOS/LAMBDA;
+    OMEGA=2*pi*135e9; %
+end;
+
 DENSCOEFF      = (ELECTRONCHARGE)^2/OMEGA/LIGHTVELOS/ELECTRONMASS;%*2*pi;
 
+denCR            =  ELECTRONMASS*OMEGA^2/(4*pi*ELECTRONCHARGE^2);
+
+
 ph=den*(DENSCOEFF*100); %*100 because working in meters
+ph=-OMEGA/LIGHTVELOS*(sqrt(1-den/denCR)-1)*100/2/pi;
--- Density/density_phase2den.m
+++ Density/density_phase2den.m
@@ -1,12 +1,19 @@
-function den=density_phase2den(ph)
+function den=density_phase2den(ph,OMEGA)
+% converts dPhase[rad/2/pi]/dL [meters]  to density 1/cm-3
 %% const in CGS
 ELECTRONCHARGE = 4.8027E-10;
 LIGHTVELOS     = 2.9979E10; 
 ELECTRONMASS   = 9.1083E-28;
 
 LAMBDA         = 0.22;%2.15E-1;%wavelength of the interferometer
-OMEGA          = 2*pi*LIGHTVELOS/LAMBDA;
-OMEGA=2*pi*136e9; %
+if nargin<2||isempty(freq)
+    LAMBDA         = 0.22;%2.15E-1;%wavelength of the interferometer
+    OMEGA          = 2*pi*LIGHTVELOS/LAMBDA;
+    OMEGA=2*pi*135e9; %
+end;
+
 DENSCOEFF      = (ELECTRONCHARGE)^2/OMEGA/LIGHTVELOS/ELECTRONMASS;%*2*pi;
+denCR            =  ELECTRONMASS*OMEGA^2/(4*pi*ELECTRONCHARGE^2);
 
 den=ph/(DENSCOEFF*100); %*100 because working in meters
+den=denCR*(1-(1-ph*2*pi*LIGHTVELOS/OMEGA/100).^2);
