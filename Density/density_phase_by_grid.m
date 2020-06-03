f1dc7a82d2586378cd77a2df5722fb3e785e9625
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-06-03 14:45:13 +0300

    phases from straight chords. initial

---
+++ Density/density_phase_by_grid.m
@@ -0,0 +1,17 @@
+function [phX,phX0]=density_phase_by_grid(x,n,freq)
+% x in cm
+
+if nargin<7||isempty(freq)
+    f=135e9;    
+else
+    f=freq;
+end;
+
+if ~iscolumn(x)
+    x=x';
+end
+
+dx=diff(x);
+[dPhdL,dPhdL0]=density_den2phase(n,f);
+phX=dPhdL(1:end-1,1:end-1)'*dx;
+phX0=dPhdL0(1:end-1,1:end-1)'*dx;
