798042aa7e969d648a5432a9d8bd81ff087b50fb
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-06-03 14:37:49 +0300

    from phase to density and another direction transform in two ways (with or without "square root")

--- Density/density_den2phase.m
+++ Density/density_den2phase.m
@@ -1,4 +1,4 @@
-function ph=density_den2phase(den,freq)
+function [ph,ph0]=density_den2phase(den,freq)
 % ph is phase addition per length unit  dPhase[rad/2/pi]/dL [meters] 
 %% const in CGS
 ELECTRONCHARGE = 4.8027E-10;
@@ -10,6 +10,8 @@ if nargin<2||isempty(freq)
     LAMBDA         = 0.22;%2.15E-1;%wavelength of the interferometer
     OMEGA          = 2*pi*LIGHTVELOS/LAMBDA;
     OMEGA=2*pi*135e9; %
+else
+    OMEGA=2*pi*freq;
 end;
 
 DENSCOEFF      = (ELECTRONCHARGE)^2/OMEGA/LIGHTVELOS/ELECTRONMASS;%*2*pi;
@@ -17,5 +19,8 @@ DENSCOEFF      = (ELECTRONCHARGE)^2/OMEGA/LIGHTVELOS/ELECTRONMASS;%*2*pi;
 denCR            =  ELECTRONMASS*OMEGA^2/(4*pi*ELECTRONCHARGE^2);
 
 
-ph=den*(DENSCOEFF*100); %*100 because working in meters
-ph=-OMEGA/LIGHTVELOS*(sqrt(1-den/denCR)-1)*100/2/pi;
+ph0=den*(DENSCOEFF*100); %*100 because working in meters
+ph0=-OMEGA/LIGHTVELOS*(sqrt(1-den/denCR)-1)*100/2/pi;
+
+ph0=den*(DENSCOEFF); %*100 because working in meters
+ph=-OMEGA/LIGHTVELOS*(sqrt(1-den/denCR)-1)/2/pi;
--- Density/density_phase2den.m
+++ Density/density_phase2den.m
@@ -1,4 +1,4 @@
-function den=density_phase2den(ph,OMEGA)
+function [den,den0]=density_phase2den(ph,OMEGA)
 % converts dPhase[rad/2/pi]/dL [meters]  to density 1/cm-3
 %% const in CGS
 ELECTRONCHARGE = 4.8027E-10;
@@ -9,11 +9,13 @@ LAMBDA         = 0.22;%2.15E-1;%wavelength of the interferometer
 if nargin<2||isempty(freq)
     LAMBDA         = 0.22;%2.15E-1;%wavelength of the interferometer
     OMEGA          = 2*pi*LIGHTVELOS/LAMBDA;
-    OMEGA=2*pi*135e9; %
+%     OMEGA=2*pi*135e9; %
 end;
 
 DENSCOEFF      = (ELECTRONCHARGE)^2/OMEGA/LIGHTVELOS/ELECTRONMASS;%*2*pi;
 denCR            =  ELECTRONMASS*OMEGA^2/(4*pi*ELECTRONCHARGE^2);
 
 den=ph/(DENSCOEFF*100); %*100 because working in meters
+den0=ph/(DENSCOEFF); %*100 because working in meters
 den=denCR*(1-(1-ph*2*pi*LIGHTVELOS/OMEGA/100).^2);
+den=denCR*(1-(1-ph*2*pi*LIGHTVELOS/OMEGA).^2);
