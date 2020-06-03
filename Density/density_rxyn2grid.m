0c3067fabc33ce3f6c89d484d9cb326583dc3b5a
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-06-03 14:38:59 +0300

    density_rxyn2grid. Faster way with griddata interpolation.

--- Density/density_rxyn2grid.m
+++ Density/density_rxyn2grid.m
@@ -15,5 +15,19 @@ x=linspace(-xdim/2,xdim/2,nx);
 y=linspace(-ydim/2,ydim/2,ny);
 [X,Y]=meshgrid(x,y);
 n=zeros(size(X));
-n(:)=interp1(rxyn(:,1),rxyn(:,4),density_xy2r(X(:),Y(:),rxyn),'pchip',0);
-%
+gridstep=min([diff(x),diff(y)]);
+Ntheta=round(2*pi*max(rxyn(:,1))/gridstep);
+Nr=size(rxyn,1);
+theta=linspace(0,2*pi,Ntheta+1);
+theta(end)=[];
+
+xx=zeros(Nr,Ntheta);
+yy=xx;
+nn=xx;
+
+for nr=1:Nr
+    nn(nr,:)=rxyn(nr,4);
+    xx(nr,:)=rxyn(nr,2)+rxyn(nr,1)*cos(theta);
+    yy(nr,:)=rxyn(nr,3)+rxyn(nr,1)*sin(theta);
+end
+n=griddata(xx,yy,nn,X,Y);
