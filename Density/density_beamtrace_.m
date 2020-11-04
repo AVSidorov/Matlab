664fd7a49d360ebf36f267c78da9a878e977ccd2
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-11-04 15:22:42 +0300

    density_beamtrace_
    Added full output not only curX curY, but Amp, curPhase and Field

--- Density/density_beamtrace_.m
+++ Density/density_beamtrace_.m
@@ -1,4 +1,4 @@
-function [curX,curY]=density_beamtrace_(x,y,n,rays,antx_TX,anty_TX,antx_RX,anty_RX,freq)
+function [ph,Amp,curX,curY,curPhase,E,Eant]=density_beamtrace_(x,y,n,rays,antx_TX,anty_TX,antx_RX,anty_RX,freq)
 % function 
 % x,y in cm
 
@@ -12,7 +12,7 @@ function [curX,curY]=density_beamtrace_(x,y,n,rays,antx_TX,anty_TX,antx_RX,anty_
 %% settings
     Nsteps=10000;
     dt=1e-13;
-    focus=0.009; %in meters
+    focus=0.0095; %in meters
 
 %% constants
     w=2*pi*f;
@@ -41,7 +41,7 @@ function [curX,curY]=density_beamtrace_(x,y,n,rays,antx_TX,anty_TX,antx_RX,anty_
     curX=zeros(Nr,Nsteps);
     curY=zeros(Nr,Nsteps);
     
-    curPhase=zeros(Nr,1);    
+    curPhase=zeros(Nr,Nsteps);    
     out=false(Nr,1);
     
     
@@ -78,7 +78,7 @@ function [curX,curY]=density_beamtrace_(x,y,n,rays,antx_TX,anty_TX,antx_RX,anty_
             end
             
             %new phasse
-            curPhase(j,i)=curPhase(j)+dx*curKy(j)+dy*curKx(j);
+            curPhase(j,i+1)=curPhase(j,i)+dx*curKy(j)+dy*curKx(j);
             
             %new end of ray
             curX(j,i+1)=curX(j,i)+dx/gridstepx;
@@ -93,9 +93,37 @@ function [curX,curY]=density_beamtrace_(x,y,n,rays,antx_TX,anty_TX,antx_RX,anty_
     end;
 %% convert from greed to meters
 ind=find(all(curX==0),1,'first');
-curX=curX(:,1:ind-1);
-curY=curY(:,1:ind-1);
- curX=interp1(1:nx,x,curX,'linear',NaN);
- curY=interp1(1:ny,y,curY,'linear',NaN);
-    
+Nstep=ind-1;
+curX=curX(:,1:Nstep);
+curY=curY(:,1:Nstep);
+curPhase=curPhase(:,1:Nstep);
+
+curX=interp1(1:nx,x,curX,'linear','extrap');
+ curY=interp1(1:ny,y,curY,'linear','extrap');
+
+%% Amplitude relative change of field & Full (complex) Field
+ ray_dist=sqrt(diff(curX).^2 + diff(curY).^2);
+ ray_dist(end+1,:)=ray_dist(end,:);
+ ray_dist(2:end-1,:)=(ray_dist(1:end-2,:)+ray_dist(2:end-1,:))/2;
+ amp=ray_dist;
+ E=amp;
+for i=1:Nstep
+    amp(:,i)=ray_dist(:,1)./ray_dist(:,i);
+    E(:,i)=rays(:,5).*amp(:,i).*exp(-1i*curPhase(:,i));
+end;
+
+[~,~,~,rayGauss]=Gauss_beam(x/100,antx_RX/100,-focus,f);
+
+Eant=griddata(curX(:),curY(:),E(:),x,anty_RX*ones(size(x)));
+Eant(isnan(Eant))=0;
+
+distint=sqrt(diff(x).^2);
+distint(end+1)=distint(end);
+distint(2:end-1)=(distint(1:end-2)+distint(2:end-1))/2;
+
+total=sum(rayGauss.*Eant.*distint*0.7);  
+
+ph=angle(total);
+Amp=abs(total);
+
 end
