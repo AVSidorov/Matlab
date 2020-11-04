1724725a562f7284ffce8a4aa232264ebd07cb7a
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-11-04 15:24:01 +0300

    density_denByPhase
    some Fixes

--- Density/density_denByPhase.m
+++ Density/density_denByPhase.m
@@ -1,6 +1,6 @@
 function [rxyn,prof,ph,phU,khi,niter]=density_denByPhase(antSet,Ph,rays,rxyn,ph,freq)
 Rdia=7.79;
-Rlcs=Rdia-0.01;
+Rlcs=7.57;
 xdim=20;
 nx=2200;
 ydim=20;
@@ -26,7 +26,7 @@ end
 if isempty(Rlcs)
     Rlcs=rxyn(find(rxyn(:,1)+sqrt(rxyn(:,2).^2+rxyn(:,3).^2)>=Rdia,1,'first'),1);
     if isempty(Rlcs)
-        Rlcs=Rdia-rxyn(end,2);
+        Rlcs=Rdia-sqrt(rxyn(end,2)^2+rxyn(end,3)^2);
     end
 end;
 rxyn(rxyn(:,1)>Rlcs,:)=[];
@@ -44,11 +44,13 @@ ph0(x>=rxyn(end,2)+Rlcs)=0;
 
 %% make initial densities by pancakes if isempty
    if all(rxyn(:,4)<1e10)
+        rxynOld=rxyn;
         [R,C,~,~,Den]=density_pancake(x,ph0,freq);
         rxyn=zeros(size(R,1),4);
         rxyn(:,[1,2,4])=[R,C,Den];
         rxyn(rxyn(:,1)>Rlcs,:)=[];
         rxyn=sortrows(rxyn);
+        rxyn(:,3)=interp1(rxynOld(:,1),rxynOld(:,3),rxyn(:,1));
    end
 %% fit
 nr=size(antSet,1); % number of beams
@@ -67,7 +69,7 @@ while niter>2
         ph1=density_phase_by_grid(y,n,freq);
 
         %% get phases beam tracing
-        [prof,ph,phU]=density_phase_by_beamtrace(x,y,n,rays,antSet,ph,[],freq);
+        [prof,ph,phU]=density_phase_by_beamtrace(x,y,n,rays,antSet,ph,[],[],freq);
 
         khi(i+1)=sum((Ph(:,2)-prof).^2);
         if khi(i+1)>khi(i)
@@ -79,8 +81,8 @@ while niter>2
         phB(x>=rxyn(end,2)+Rlcs)=0;
 
         %% make dPh
-        dPh=ph0-phB;
-
+         dPh=ph0-phB;
+        % dPh=csaps([rxyn(end,2)-Rlcs;Ph(1:nr,1);rxyn(end,2)+Rlcs],[0;Ph(:,2)-prof;0],1,x); % gives same result               
         %% change ph1 and make new rxyn;
         ph1=reshape(ph1,1,[]);
         ph1=ph1+k*dPh;
@@ -89,6 +91,7 @@ while niter>2
         rxynOld=rxyn;
         rxyn=zeros(size(R,1),4);
         rxyn(:,[1,2,4])=[R,C,Den];
+        rxyn(:,3)=interp1(rxynOld(:,1),rxynOld(:,3),rxyn(:,1),'linear',mean(rxynOld(:,3)));
         rxyn(rxyn(:,1)>Rlcs,:)=[];
         rxyn=sortrows(rxyn);
         if rxyn(end,1)~=Rlcs
