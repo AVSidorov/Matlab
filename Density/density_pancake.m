941742f7e9b658eb5828f00f697bc873661dedfe
Anton Sidorov <sidorov@mail.ioffe.ru>
2020-11-14 18:42:44 +0300

    density_pancake. Refactored.
    Taking in account limited calculation accuracy.
    Reducing more than two points

--- Density/density_pancake.m
+++ Density/density_pancake.m
@@ -7,47 +7,73 @@ x=reshape(x,[],1);
 fx=fx(index);
 fx=reshape(fx,[],1);
 
+%we should reduce Radii to avoid "touching" of circles
+    dr=min([10*eps,min(diff(x))/numel(x)]);
 %phase must have zeros at ends
+    if fx(1)~=0
+        x=[x(1)-2*dr;x];
+        fx=[0;fx];
+    end
+    if fx(end)~=0
+        x=[x;x(end)+2*dr];
+        fx=[fx;0];
+    end
+%preallocation    
 n=numel(x);
 R=zeros(n,1);
 C=zeros(n,1);
 dPhdL=zeros(n,1);
-
-%we should reduce Radii to avoid "touching" of circles
-    dr=min([10*eps,min(diff(x))/n]);
+    
     i=0;
-while numel(x)>1 % "while" instead "for" because two points can be reduced
+while numel(x)>1 % "while" instead "for" because two or more points can be reduced
+    % on each step we leave only one zero point on each end
+    stI=find(abs(fx)>eps,1,'first')-1;
+    endI=find(abs(fx)>eps,1,'last')+1;
+    % abs(fx)>eps due to limited accuracy  fx(2)or/and fx(end-1) may be not
+    % exact zero
+    x=x(stI:endI);
+    fx=fx(stI:endI);   
+    if isempty(x)
+        break;
+    end;
+    %make ends exact zero
+    fx(1)=0;
+    fx(end)=0;    
     i=i+1;
     R(i)=(x(end)-x(1))/2-dr*i;
     C(i)=(x(end)+x(1))/2;
+    if numel(x)<2
+        break;
+    end;    
     %make pancake chord lengths
     L=2*sqrt(R(i)^2-(x(2:end-1)-C(i)).^2); % 2:end-1 to avoid negative value under root and L on circle borders (or out side) is zero
     %determine dPhdL on current pancake
-    [dPhdL(i),ind]=min([fx(2)/L(1) fx(end-1)/L(end)]);
+    [dPhdL(i),ind]=min([fx(2)/L(1) fx(end-1)/L(end)]);    
     %make new x and fx subtract pancake and reduce ends.
     % new fx subtract pancake
     fx(2:end-1)=fx(2:end-1)-L.*dPhdL(i);
-    % on each step we reduce either one point or two 
-    if fx(2)/L(1)==fx(end-1)/L(end)
-        x([1,end])=[];    %reduce two points
-        fx([1,end])=[];
-    elseif ind==1
-        x(1)=[];
-        fx(1)=[];
-    else
-        x(end)=[];
-        fx(end)=[];
-    end
 end
-C(i+1)=x;
-R(i+1)=max([0 R(i)+max(diff(R(1:i))) R(i)/2]); %dR is negative take min by abs
-if i+1<n
-    R(i+2:end)=[];
-    C(i+2:end)=[];
-    dPhdL(i+2:end)=[];
+if i<n
+    R(i+1:end)=[];
+    C(i+1:end)=[];
+    dPhdL(i+1:end)=[];
 end
     
 denPh=cumsum(dPhdL);
 denPh(denPh<0)=0;
 Den=density_phase2den(denPh,freq);
+end
+function [l,x,y]=ChordLength(x,r,t,e)
+% calculates chord length surface radius r, triangularity t and elongation e
+% x distance to center
+    if t~=0
+        c=(1-sqrt(1+4*t*(x/r+t)))/2/t;
+        x(abs(c)>1)=[];
+        c(abs(c)>1)=[];
+    else
+        c=x/r;
+    end
+    
+    y=r*e*sqrt(1-c.^2);
+    l=2*y;    
 end
