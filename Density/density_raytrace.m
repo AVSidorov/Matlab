26dc7663d96b8a6fbf64cdc2b7d433689ca55e2d
Anton Sidorov <sidorov@mail.ioffe.ru>
2021-02-16 00:48:38 +0300

    density_raytrace. Initial commit. Raytracing with integration by dl

---
+++ Density/density_raytrace.m
@@ -0,0 +1,127 @@
+function [curX,curY,curKx,curKy,curL,curPhase]=density_raytrace(x,y,n,x0,y0,kx0,ky0,freq)
+% x,y in cm
+%% input check
+    if nargin<8||isempty(freq)
+        f=135e9;    
+    else
+        f=freq;
+    end;
+%% k normalization
+ k=norm([kx0 ky0]);
+ kx0=kx0/k;
+ ky0=ky0/k;
+%% constants
+    w=2*pi*f;
+    c=299792458*100;
+    e=4.803e-10;
+    me=9.10938356e-28;
+    nc=me*w^2/(4*pi*e^2);
+%% grid size
+    nx=length(x);
+    ny=length(y);
+    
+%% step grid
+    x=reshape(x,1,[]); %vectors to row
+    y=reshape(y,1,[]);
+    dX=diff(x);
+    dY=diff(y);
+    [DX,DY]=meshgrid(dX,dY);
+    
+%% gradient
+    %[dNx,dNy]=gradient(n,x,y);
+    dNx=(n(1:end-1,2:end)-n(1:end-1,1:end-1))./DX;
+    dNy=(n(2:end,1:end-1)-n(1:end-1,1:end-1))./DY;
+    
+%% preallocation
+    Nstep=2*max(nx+1,ny+1);
+    curX=zeros(Nstep,1);
+    curY=zeros(Nstep,1);
+    curKx=zeros(Nstep,1);
+    curKy=zeros(Nstep,1);
+    curL=zeros(Nstep,1);
+    curPhase=zeros(Nstep,1);
+    
+    x2left=[-1 0];
+    x2right=[0 1];
+    
+    y2down=[-1 0];
+    y2up=[0,1];
+%%  initialization of the rays
+    curX(1)=interp1(x,1:nx,x0,'linear',1);
+    curY(1)=interp1(y,1:ny,y0,'linear',1);
+    curKx(1)=kx0*w/c;
+    curKy(1)=ky0*w/c;    
+
+    
+%% actual raytracing from cell to cell
+    i=1;
+    while 1<curX(i)&&curX(i)<nx&&1<curY(i)&&curY(i)<ny %coordinates in grid units
+        %% current values         
+        kX=fix(curX(i));
+        kY=fix(curY(i));
+        
+        gradNx=dNx(kY,kX);
+        gradNy=dNy(kY,kX);
+        
+        if curX(i)-kX==0&&curKx(i)<0
+            hx=dX(kX-1);
+        else            
+            hx=dX(kX); 
+        end    
+        if curY(i)-kY==0&&curKy(i)<0
+            hy=dY(kY-1);
+        else            
+            hy=dY(kY);
+        end
+
+        %% determination distance to nearest cell border in grid units  
+        x2left(2)=kX-curX(i); %moving inside shifted grid cell
+        x2right(1)=1+kX-curX(i);
+        
+        y2down(2)=kY-curY(i);
+        y2up(1)=1+kY-curY(i);
+        
+        
+        %% founding shortest path length till cell border in physical units 
+        l=zeros(0,1);
+        
+        l=[l;roots([-gradNx/2/nc/2,curKx(i)*c/w,-max(x2left(x2left<0))*hx])];
+        l=[l;roots([-gradNx/2/nc/2,curKx(i)*c/w,-min(x2right(x2right>0))*hx])];
+            
+        l=[l;roots([-gradNy/2/nc/2,curKy(i)*c/w,-max(y2down(y2down<0))*hy])];
+        l=[l;roots([-gradNy/2/nc/2,curKy(i)*c/w,-min(y2up(y2up>0))*hy])];
+        
+        l=l(~imag(l(:)));
+        l=l(l>0);
+        if isempty(l)
+            break;
+        else
+            curL(i)=min(l);
+            curKx(i+1)=curKx(i)-gradNx*w/2/nc/c*curL(i);
+            curKy(i+1)=curKy(i)-gradNy*w/2/nc/c*curL(i);
+            
+            %convert physical length to grid coordinates
+            %in main grid 
+            curX(i+1)=curX(i)+polyval([-gradNx/2/nc/2,curKx(i)*c/w,0],curL(i))/hx; 
+            curY(i+1)=curY(i)+polyval([-gradNy/2/nc/2,curKy(i)*c/w,0],curL(i))/hy;
+            
+            
+            curPhase(i+1)=curPhase(i)+curL(i)*w/c;
+            i=i+1;
+            %snaprounding to shifted grid first time at step 2
+             if abs(curX(i)-fix(curX(i)))<abs(curY(i)-fix(curY(i)))
+                 curX(i)=round(curX(i));
+             else
+                 curY(i)=round(curY(i));
+             end;
+
+        end;
+    end;
+    curX=curX(1:i);
+    curY=curY(1:i);
+    curX=interp1(1:nx,x,curX,'linear','extrap');
+    curY=interp1(1:ny,y,curY,'linear','extrap');
+    curKx=curKx(1:i);
+    curKy=curKy(1:i);
+    curL=curL(1:i);
+end
